297fc2791d659b2ce2b6a39fd691b0f518fde342
Andy <duyanhnn@outlook.com>
2024-08-24 10:32:58 +0700

    commit

--- src/main/java/pkg/array/Ex0Practice.java
+++ src/main/java/pkg/array/Ex0Practice.java
@@ -5,13 +5,23 @@ import org.apache.commons.lang3.ArrayUtils;
 public class Ex0Practice
 {
     // Practice
-    // Given an array, left side is <= N, right side is > N
-    // Example: 1, 6, 9, 3, 5, 10, 13 and N = 7
+    // Given an array, left side is <= M, right side is > M
+    // Example: 1, 6, 9, 3, 5, 10, 13 and M = 7
     // Expected: 1, 3, 5, 6 on left side, 9, 10, 13 on right side
-    public static int[] classify(int arr[], int n)
+    public static int classify(int arr[], int m)
     {
-
-        return arr;
+        int trace = arr.length;
+        for (int i = arr.length - 1; i > -1; i--)
+        {
+            if (arr[i] > m)
+            {
+                trace--;
+                int temp = arr[i];
+                arr[i] = arr[trace];
+                arr[trace] = temp;
+            }
+        }
+        return trace;
     }
 
 
@@ -19,15 +29,18 @@ public class Ex0Practice
     {
         int arr[];
         int n;
+        int boundary;
 
         arr = new int[] { 1, 6, 9, 3, 5, 10, 13 };
         n = 7;
-        System.out.println(ArrayUtils.toString(arr));
-        System.out.println(ArrayUtils.toString(classify(arr, n)));
+        System.out.println(ArrayUtils.toString(arr) + ". Midpoint: " + n);
+        boundary = classify(arr, n);
+        System.out.println(ArrayUtils.toString(arr) + ". Boundary index: " + boundary + ". Boundary value: " + arr[boundary]);
 
         arr = new int[] { 1, 6, 9, 3, 5, 10, 13, 7 };
         n = 7;
-        System.out.println(ArrayUtils.toString(arr));
-        System.out.println(ArrayUtils.toString(classify(arr, n)));
+        System.out.println(ArrayUtils.toString(arr) + ". Midpoint: " + n);
+        boundary = classify(arr, n);
+        System.out.println(ArrayUtils.toString(arr) + ". Boundary index: " + boundary + ". Boundary value: " + arr[boundary]);
     }
 }
