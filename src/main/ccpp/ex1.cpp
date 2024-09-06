b3f0ec3892328b55b561b27fd0f1cb33e3bb46d1
Andy Nguyen <andy.nguyen@championdata.com.au>
2024-09-06 21:23:27 +0700

    commit

---
+++ src/main/ccpp/ex1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <stack>
+#include <queue>
+#include <list>
+#include <regex>
+#include <algorithm>
+#include <cmath>
+#include <math.h>
+#include <string>
+#include <sstream>
+#include <functional>
+#include <cstdio>
+#include <climits>
+#include <limits>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    return 0;
+}
