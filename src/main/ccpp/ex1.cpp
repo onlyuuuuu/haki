e4c87d06a7dc436d28cbf709c21f27346566c993
Andy <duyanhnn@outlook.com>
2024-09-18 21:56:16 +0700

    commit

--- src/main/ccpp/ex1.cpp
+++ src/main/ccpp/ex1.cpp
@@ -21,5 +21,19 @@ int main()
 {
     int n;
     cin >> n;
-    return 0;
+    int h;
+    cin >> h;
+    int total = 0;
+    int ih;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> ih;
+        if (ih > h)
+        {
+            total += 2;
+            continue;
+        }
+        ++total;
+    }
+    cout << total << endl;
 }
