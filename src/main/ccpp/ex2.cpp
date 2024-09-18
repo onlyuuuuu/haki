df782a27de89a0fbf7881a42cf2a84281ff7db7e
Andy Nguyen <andy.nguyen@championdata.com.au>
2024-09-18 22:52:54 +0700

    commit

---
+++ src/main/ccpp/ex2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <stack>
+#include <queue>
+#include <list>
+#include <regex>
+#include <algorithm>
+#include <cmath>
+#include <math.h>
+#include <string>
+#include <sstream>
+#include <functional>
+#include <cstdio>
+#include <climits>
+#include <limits>
+
+using namespace std;
+
+int main()
+{
+	int weight;
+	cin >> weight;
+	if (weight <= 2)
+	{
+		cout << "NO" << endl;
+		return 0;
+	}
+	if (weight % 2 == 0)
+	{
+		cout << "YES" << endl;
+		return 0;
+	}
+	cout << "NO" << endl;
+}
