b50413a51845c308a691568d0fa10f9813c6c18f
Andy <duyanhnn@outlook.com>
2024-09-24 10:50:57 +0700

    commit

--- src/main/ccpp/01/ex2.cpp
+++ src/main/ccpp/01/ex2.cpp
@@ -20,5 +20,33 @@ using namespace std;
 
 int main()
 {
-    
+    int n;
+    cin >> n;
+
+    int a[n + 2];
+    a[0] = 0;
+    a[n + 1] = 0;
+
+    int l[n + 2];
+    l[0] = 0;
+    l[n + 1] = 0;
+
+    int r[n + 2];
+    r[0] = 0;
+    r[n + 1] = 0;
+
+    stack<int> lst;
+    lst.push(0);
+
+    stack<int> rst;
+    rst.push(n + 1);
+
+    for (int i = 1; i <= n; ++i)
+    {
+        cin >> a[i];
+        while (a[lst.top()] >= a[i])
+            lst.pop();
+        l[i] = lst.top();
+        lst.push(a[i]);
+    }
 }
