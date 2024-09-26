798501513d152eee7b8d7c05bbb71b98fddc6e18
Andy <duyanhnn@outlook.com>
2024-09-26 21:30:38 +0700

    commit

---
+++ src/main/ccpp/02/ex1_again_0.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <stack>
+#include <queue>
+#include <list>
+#include <set>
+#include <regex>
+#include <algorithm>
+#include <cmath>
+#include <math.h>
+#include <string>
+#include <sstream>
+#include <functional>
+#include <cstdio>
+#include <climits>
+#include <limits>
+
+using namespace std;
+
+int main()
+{
+    int i, j, k;
+    int numberOfTests;
+    int numberOfJudges;
+    int numberOfOppPerFight;
+    int left, right;
+    string result = "";
+
+    cin >> numberOfTests;
+    for (i = 0; i < numberOfTests; ++i)
+    {
+        // init
+        cin >> numberOfJudges;
+        cin >> numberOfOppPerFight;
+        int apm[numberOfJudges];
+
+        // TODO: This should be done exactly like ex2 but too lazy for it
+
+        result.append(" ");
+
+        result.append("\n");
+    }
+    cout << result << endl;
+}
