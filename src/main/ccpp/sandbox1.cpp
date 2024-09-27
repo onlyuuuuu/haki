ff4aba9dc50b2150b54d6de1c5bc86f645237c74
Andy Nguyen <andy.nguyen@championdata.com.au>
2024-09-27 23:08:40 +0700

    commit

--- src/main/ccpp/sandbox1.cpp
+++ src/main/ccpp/sandbox1.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define NEW_LINE "\n"
 string output = "";
 int temp, max, min, total, avg, ans, result;
 int i, j, k, l, m, n;
@@ -12,11 +11,20 @@ static priority_queue<int, vector<int>, decltype(compareForMax)> maxIndexHeap(co
 static priority_queue<int, vector<int>, decltype(compareForMin)> minIndexHeap(compareForMin);
 static priority_queue<int> maxHeap;
 static priority_queue<int, vector<int>, greater<int>> minHeap;
+#define NEW_LINE "\n";
+#define clearVector(v,size) v.clear(); v = vector<int>(size);
+#define clearMaxHeap(mh) mh = priority_queue<int>();
+#define clearMinHeap(mh) mh = priority_queue<int, vector<int>, greater<int>>();
 static auto _ = []() { ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); return NULL; }();
 
 int main()
 {
-
+    vector<int> v(10);
+    for (i = 0; i < 10; i++) v[i] = i + 1;
+    for (i = 0; i < 10; i++) cout << v[i] << " ";
+    clearVector(v, 10);
+    cout << endl << v.size() << endl;
+    cout << v[0] << endl;
 
     cout << output;
     return 0;
