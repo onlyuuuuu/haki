39aac39656bab62a06350b145efd9166365586a5
Andy <duyanhnn@outlook.com>
2024-10-05 13:28:28 +0700

    commit

---
+++ src/main/java/Sandbox.java
@@ -0,0 +1,13 @@
+import java.util.TreeSet;
+
+public class Sandbox
+{
+    public static void main(String[] args)
+    {
+        TreeSet<Integer> numbers = new TreeSet<>();
+        numbers.add(1);
+        numbers.add(5);
+        numbers.add(3);
+        numbers.add(2);
+    }
+}
