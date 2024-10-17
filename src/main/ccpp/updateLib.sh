05ceb4bfc8e6665fdf8a6f2c313b4bdfd29f3423
Andy <duyanhnn@outlook.com>
2024-10-17 21:45:51 +0700

    .

--- src/main/ccpp/updateLib.sh
+++ src/main/ccpp/updateLib.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 sudo mkdir -p /Library/Developer/CommandLineTools/usr/include/c++/v1
-sudo rm -rf /Library/Developer/CommandLineTools/usr/include/c++/v1/bits/
 sudo cp -R bits /Library/Developer/CommandLineTools/usr/include/c++/v1/bits
 
-sudo rm -rf /Library/Developer/CommandLineTools/usr/include/bits/
 sudo cp -R bits /Library/Developer/CommandLineTools/usr/include/bits
 
 sudo mkdir -p /usr/local/include
-sudo rm -rf /usr/local/include/bits/
 sudo cp -R bits /usr/local/include/bits
