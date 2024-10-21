4ed24b5cc7f251c2d4e0440e2c39b3a1cf573cae
Andy <duyanhnn@outlook.com>
2024-10-21 22:13:38 +0700

    commit

--- src/main/ccpp/updateLib.sh
+++ src/main/ccpp/updateLib.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-sudo mkdir -p /Library/Developer/CommandLineTools/usr/include/c++/v1
-sudo cp -R bits /Library/Developer/CommandLineTools/usr/include/c++/v1/bits
+sudo mkdir -p /Library/Developer/CommandLineTools/usr/include/c++/v1/bits
+sudo cp bits/stdc++.h /Library/Developer/CommandLineTools/usr/include/c++/v1/bits/
 
-sudo cp -R bits /Library/Developer/CommandLineTools/usr/include/bits
+sudo mkdir -p /Library/Developer/CommandLineTools/usr/include/bits
+sudo cp bits/stdc++.h /Library/Developer/CommandLineTools/usr/include/bits/
 
-sudo mkdir -p /usr/local/include
-sudo cp -R bits /usr/local/include/bits
+sudo mkdir -p /usr/local/include/bits
+sudo cp bits/stdc++.h /usr/local/include/bits/
+
+sudo mkdir -p /opt/homebrew/Cellar/llvm/19.1.2/include/c++/v1/bits
+sudo cp bits/stdc++.h /opt/homebrew/Cellar/llvm/19.1.2/include/c++/v1/bits/
+
+sudo mkdir -p /usr/local/opt/llvm/include/c++/v1/bits
+sudo cp bits/stdc++.h /usr/local/opt/llvm/include/c++/v1/bits/
