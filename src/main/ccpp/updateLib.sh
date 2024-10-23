152aa1e40f43fa3f488401535cf92ae04e9166f3
Andy <duyanhnn@outlook.com>
2024-10-23 18:28:27 +0700

    commit

--- src/main/ccpp/updateLib.sh
+++ src/main/ccpp/updateLib.sh
@@ -3,14 +3,32 @@
 sudo mkdir -p /Library/Developer/CommandLineTools/usr/include/c++/v1/bits
 sudo cp bits/stdc++.h /Library/Developer/CommandLineTools/usr/include/c++/v1/bits/
 
+sudo mkdir -p /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1/bits
+sudo cp bits/stdc++.h /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1/bits/
+
+sudo mkdir -p /Library/Developer/CommandLineTools/SDKs/MacOSX15.0.sdk/usr/include/bits
+sudo cp bits/stdc++.h /Library/Developer/CommandLineTools/SDKs/MacOSX15.0.sdk/usr/include/bits/
+
 sudo mkdir -p /Library/Developer/CommandLineTools/usr/include/bits
 sudo cp bits/stdc++.h /Library/Developer/CommandLineTools/usr/include/bits/
+
+sudo mkdir -p /Library/Developer/CommandLineTools/usr/include/bits
+sudo cp bits/stdc++.h /Library/Developer/CommandLineTools/usr/include/bits/
+
+sudo mkdir -p /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/bits
+sudo cp bits/stdc++.h /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/bits/
+
+sudo mkdir -p /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/bits
+sudo cp bits/stdc++.h /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/bits/
+
+sudo mkdir -p /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/bits
+sudo cp bits/stdc++.h /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/bits/
 
 sudo mkdir -p /usr/local/include/bits
 sudo cp bits/stdc++.h /usr/local/include/bits/
 
-sudo mkdir -p /opt/homebrew/Cellar/llvm/19.1.2/include/c++/v1/bits
-sudo cp bits/stdc++.h /opt/homebrew/Cellar/llvm/19.1.2/include/c++/v1/bits/
+sudo mkdir -p /usr/include/bits
+sudo cp bits/stdc++.h /usr/include/bits/
 
-sudo mkdir -p /usr/local/opt/llvm/include/c++/v1/bits
-sudo cp bits/stdc++.h /usr/local/opt/llvm/include/c++/v1/bits/
+sudo mkdir -p /usr/lib/bits
+sudo cp bits/stdc++.h /usr/lib/bits/
