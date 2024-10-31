f8737c175ab2fac278057d44f32a7af9d854c07a
Andy <duyanhnn@outlook.com>
2024-10-31 08:10:28 +0700

    commit

---
+++ src/main/ccpp/C/build.sh
@@ -0,0 +1,36 @@
+#/bin/bash
+
+sudo apt install -y gcc \
+                    gcc-arm-linux-gnueabi \
+                    gcc-arm-linux-gnueabihf \
+                    gdb \
+                    gdb-multiarch \
+                    qemu-user \
+                    qemu-user-static \
+                    qemu-system \
+                    make \
+                    cmake \
+                    git \
+                    flex \
+                    bison \
+                    bc \
+                    libncurses-dev \
+                    exuberant-ctags \
+                    libssl-dev \
+                    binutils \
+                    util-linux \
+                    e2fsprogs \
+                    jfsutils \
+                    reiserfsprogs \
+                    xfsprogs \
+                    squashfs-tools \
+                    btrfs-progs \
+                    pcmciautils \
+                    quota \
+                    ppp \
+                    procps \
+                    oprofile \
+                    udev \
+                    grub2-common \
+                    iptables \
+                    python3-sphinx
