95865f5b1a198e02eb73098aef2794370cc1c94e
Andy <duyanhnn@outlook.com>
2024-10-31 18:05:11 +0700

    commit

--- src/main/ccpp/C/init.sh
+++ src/main/ccpp/C/init.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #/bin/bash
 
 sudo apt install -y --allow-insecure-repositories   gcc \
@@ -37,7 +39,28 @@ sudo apt install -y --allow-insecure-repositories   gcc \
                                                     java \
                                                     perl
 
-mkdir -p kernel/armhf/
-rm -rf kernel/armhf/*
+git clone https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git > /dev/null 2>&1 &
 
-make O=$(pwd)/kernel/armhf -C $(pwd)/linux ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- multi_v7_defconfig
+sudo mkdir -p /tools && sudo chown -R zero:zero /tools
+sudo mkdir -p /x && sudo chown -R zero:zero /x
+sudo mkdir -p /y && sudo chown -R zero:zero /y
+sudo mkdir -p /z && sudo chown -R zero:zero /z
+sudo mkdir -p /0 && sudo chown -R zero:zero /0
+
+cd /tools
+if [[ "$(uname -p)" == "aarch64" ]]; then
+    curl -L -o code.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-arm64'
+else
+    curl -L -o code.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'
+fi
+sudo dpkg -i code.deb
+
+if [[ "$(uname -p)" == "aarch64" ]]; then
+    curl -L -o toolbox.tar.gz 'https://download.jetbrains.com/toolbox/jetbrains-toolbox-2.5.1.34629-arm64.tar.gz?_gl=1*1ntrigw*_gcl_au*MTE5NjA2NTU2Ni4xNzI0MzgwMDMx*_ga*MTkwODQ3NDU5My4xNjY0ODk2NjIz*_ga_9J976DJZ68*MTczMDM3MjA1OS4xMDcuMS4xNzMwMzcyMjIzLjU5LjAuMA..'
+else
+    curl -L -o toolbox.tar.gz 'https://download.jetbrains.com/toolbox/jetbrains-toolbox-2.5.1.34629.tar.gz?_gl=1*1g5a3bm*_gcl_au*MTE5NjA2NTU2Ni4xNzI0MzgwMDMx*_ga*MTkwODQ3NDU5My4xNjY0ODk2NjIz*_ga_9J976DJZ68*MTczMDM3MjA1OS4xMDcuMS4xNzMwMzcyMjQ4LjM0LjAuMA..'
+fi
+tar xzvf toolbox.tar.gz
+find . -type d -name "jetbrains-toolbox-*" -exec mv {}/jetbrains-toolbox jetbrains-toolbox \; > /dev/null 2>&1
+find . -type d -name "jetbrains-toolbox-*" -exec rm -rf {} \; > /dev/null 2>&1
+./jetbrains-toolbox
