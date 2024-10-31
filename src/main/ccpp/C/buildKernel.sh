ca35137cbf25b24aae72e92bada4adbd5471903f
Andy <duyanhnn@outlook.com>
2024-10-31 17:23:04 +0700

    commit

--- src/main/ccpp/C/buildKernel.sh
+++ src/main/ccpp/C/buildKernel.sh
@@ -1,41 +1,41 @@
 #/bin/bash
 
-sudo apt install -y gcc \
-                    gcc-arm-linux-gnueabi \
-                    gcc-arm-linux-gnueabihf \
-                    gdb \
-                    gdb-multiarch \
-                    qemu-user \
-                    qemu-user-static \
-                    qemu-system \
-                    make \
-                    cmake \
-                    git \
-                    flex \
-                    bison \
-                    bc \
-                    libncurses-dev \
-                    exuberant-ctags \
-                    libssl-dev \
-                    binutils \
-                    util-linux \
-                    e2fsprogs \
-                    jfsutils \
-                    reiserfsprogs \
-                    xfsprogs \
-                    squashfs-tools \
-                    btrfs-progs \
-                    pcmciautils \
-                    quota \
-                    ppp \
-                    procps \
-                    oprofile \
-                    udev \
-                    grub2-common \
-                    iptables \
-                    python3-sphinx
+sudo apt install -y --allow-insecure-repositories   gcc \
+                                                    gcc-arm-linux-gnueabi \
+                                                    gcc-arm-linux-gnueabihf \
+                                                    gdb \
+                                                    gdb-multiarch \
+                                                    qemu-user \
+                                                    qemu-user-static \
+                                                    qemu-system \
+                                                    make \
+                                                    cmake \
+                                                    git \
+                                                    flex \
+                                                    bison \
+                                                    bc \
+                                                    libncurses-dev \
+                                                    exuberant-ctags \
+                                                    libssl-dev \
+                                                    binutils \
+                                                    util-linux \
+                                                    e2fsprogs \
+                                                    jfsutils \
+                                                    reiserfsprogs \
+                                                    xfsprogs \
+                                                    squashfs-tools \
+                                                    btrfs-progs \
+                                                    pcmciautils \
+                                                    quota \
+                                                    ppp \
+                                                    procps \
+                                                    oprofile \
+                                                    udev \
+                                                    grub2-common \
+                                                    iptables \
+                                                    python3-sphinx
 
 mkdir -p kernel/armhf/
 rm -rf kernel/armhf/*
 
-make O=kernel/armhf -C linux ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- multi_v7_defconfig
+make O=$(pwd)/kernel/armhf -C $(pwd)/linux ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- multi_v7_defconfig
