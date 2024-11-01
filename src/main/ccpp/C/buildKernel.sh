98a82d380e5e3e104b551a250b82e2ff8a7bc023
Andy <duyanhnn@outlook.com>
2024-11-01 10:16:31 +0700

    commit

--- src/main/ccpp/C/buildKernel.sh
+++ src/main/ccpp/C/buildKernel.sh
@@ -1,6 +1,16 @@
 #/bin/bash
 
+BASE_DIR=$(pwd)
+
 mkdir -p kernel/armhf/
 rm -rf kernel/armhf/*
 
-make O=$(pwd)/kernel/armhf -C $(pwd)/linux ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- multi_v7_defconfig
+cd linux
+
+make zImage O=$BASE_DIR/kernel/armhf -C $BASE_DIR/linux ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- multi_v7_defconfig
+make dtbs O=$BASE_DIR/kernel/armhf -C $BASE_DIR/linux ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- multi_v7_defconfig
+
+cd $BASE_DIR
+
+cp -rf $BASE_DIR/kernel/armhf/arch/arm/boot/dts/arm/vexpress-v2p-ca15-tc1.dtb $BASE_DIR/vexpress-v2p-ca15-tc1.dtb
+dtc -q -I dtb -O dts -o vexpress-v2p-ca15-tc1.dts vexpress-v2p-ca15-tc1.dtb
