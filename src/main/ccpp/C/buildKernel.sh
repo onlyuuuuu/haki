b14486204cbac9a8e615f7d0c12b0e0a226c4e5d
Andy <duyanhnn@outlook.com>
2024-11-03 00:17:13 +0700

    commit

--- src/main/ccpp/C/buildKernel.sh
+++ src/main/ccpp/C/buildKernel.sh
@@ -2,15 +2,25 @@
 
 BASE_DIR=$(pwd)
 
+echo 0 | sudo echo "Gained root session"
+
 mkdir -p kernel/armhf/
 rm -rf kernel/armhf/*
 
 cd linux
 
-make zImage O=$BASE_DIR/kernel/armhf -C $BASE_DIR/linux ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- multi_v7_defconfig
-make dtbs O=$BASE_DIR/kernel/armhf -C $BASE_DIR/linux ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- multi_v7_defconfig
+git pull
+
+make O=$BASE_DIR/kernel/armhf -C $BASE_DIR/linux ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- multi_v7_defconfig
+make O=$BASE_DIR/kernel/armhf -C $BASE_DIR/linux ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-
+echo 0 | sudo echo "Gained root session"
+sudo make O=$BASE_DIR/kernel/armhf -C $BASE_DIR/linux ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- modules_install install
 
 cd $BASE_DIR
 
 cp -rf $BASE_DIR/kernel/armhf/arch/arm/boot/dts/arm/vexpress-v2p-ca15-tc1.dtb $BASE_DIR/vexpress-v2p-ca15-tc1.dtb
 dtc -q -I dtb -O dts -o vexpress-v2p-ca15-tc1.dts vexpress-v2p-ca15-tc1.dtb
+
+#qemu-img create -f raw rootfs.ext4
+dd if=/dev/zero of=rootfs.ext4 bs=4k count=2097152
+mkfs.ext4 rootfs.ext4
