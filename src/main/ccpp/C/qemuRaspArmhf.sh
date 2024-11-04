821056c46799a47018cd26d9450ed93d6f7468b2
Andy <duyanhnn@outlook.com>
2024-11-04 22:22:48 +0700

    commit

--- src/main/ccpp/C/qemuRaspArmhf.sh
+++ src/main/ccpp/C/qemuRaspArmhf.sh
@@ -5,10 +5,9 @@ echo 0 | sudo -S qemu-system-arm \
   -cpu cortex-a7 \
   -smp 4 \
   -m 1G \
-  -sd 2024-10-22-raspios-bookworm-armhf-lite.img \
+  -drive if=sd,format=raw,file=2024-10-22-raspios-bookworm-armhf-lite.img \
   -dtb bcm2709-rpi-2-b.dtb \
   -kernel kernel7.img \
-  -netdev user,id=net0,hostfwd=tcp::2222-:22 \
-  -device usb-net,netdev=net0 \
   -append "rw earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 rootdelay=1" \
-  -no-reboot
+  -no-reboot \
+  -nographic
