83261f5773aa2aa91391f9de530ea5b03a441f1b
Andy <duyanhnn@outlook.com>
2024-11-05 20:59:46 +0700

    commit

--- src/main/ccpp/C/qemuRaspArmhf.sh
+++ src/main/ccpp/C/qemuRaspArmhf.sh
@@ -6,6 +6,8 @@ echo 0 | sudo -S qemu-system-arm -D raspi2b.debug.log \
   -dtb kernel/raspberrypi/armhf/arch/arm/boot/dts/broadcom/bcm2709-rpi-2-b.dtb \
   -kernel kernel7.img \
   -append "rw earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 rootdelay=1" \
+  -netdev user,id=net0,hostfwd=tcp::2222-:22 \
+  -device usb-net,netdev=net0 \
   -no-reboot \
   -nographic
 
