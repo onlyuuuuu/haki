2ca7900100676def7fbb5ba34599e2ef0032bbd3
Andy <duyanhnn@outlook.com>
2024-11-06 16:05:28 +0700

    commit

--- src/main/ccpp/C/prepRaspArmhf.sh
+++ src/main/ccpp/C/prepRaspArmhf.sh
@@ -44,19 +44,19 @@ echo 0 | sudo -S rm -rf mnt/boot/*
 echo 0 | sudo -S rm -rf mnt/root/*
 echo 0 | sudo -S umount mnt/boot 2> /dev/null
 echo 0 | sudo -S mount -o loop,offset=1048576 ubuntu-22.04.5-preinstalled-server-armhf+raspi.img mnt/boot
-echo 0 | sudo -S cp $BASE_DIR/mnt/boot/$KERNEL.img $BASE_DIR/mnt/boot/$KERNEL-backup.img
-echo 0 | sudo -S cp $BASE_DIR/kernel/raspberrypi/armhf/arch/arm/boot/zImage $BASE_DIR/mnt/boot/$KERNEL.img
-echo 0 | sudo -S cp $BASE_DIR/kernel/raspberrypi/armhf/arch/arm/boot/dts/broadcom/*.dtb $BASE_DIR/mnt/boot/
-echo 0 | sudo -S cp $BASE_DIR/kernel/raspberrypi/armhf/arch/arm/boot/dts/overlays/*.dtb* $BASE_DIR/mnt/boot/overlays/
-echo 0 | sudo -S touch mnt/boot/ssh
-echo 0 | sudo -S touch mnt/boot/userconf.txt
-echo 0 | sudo -S touch mnt/boot/config.txt
-echo 0 | sudo -S echo 'zero:$6$nKazTU9UYkZ15LbS$aKazZgSp5z/ynlHPrDeaE7x5h4AF3bkioe0cJ02eGR7P75HEEOwaJGcunbh/bCzJipC/6UTT/Lh5KC8lhRaJP0' | sudo tee mnt/boot/userconf.txt
-echo 0 | sudo -S echo 'arm_64bit=0' | sudo tee -a mnt/boot/config.txt
+# echo 0 | sudo -S cp $BASE_DIR/mnt/boot/$KERNEL.img $BASE_DIR/mnt/boot/$KERNEL-backup.img
+# echo 0 | sudo -S cp $BASE_DIR/kernel/raspberrypi/armhf/arch/arm/boot/zImage $BASE_DIR/mnt/boot/$KERNEL.img
+# echo 0 | sudo -S cp $BASE_DIR/kernel/raspberrypi/armhf/arch/arm/boot/dts/broadcom/*.dtb $BASE_DIR/mnt/boot/
+# echo 0 | sudo -S cp $BASE_DIR/kernel/raspberrypi/armhf/arch/arm/boot/dts/overlays/*.dtb* $BASE_DIR/mnt/boot/overlays/
+# echo 0 | sudo -S touch mnt/boot/ssh
+# echo 0 | sudo -S touch mnt/boot/userconf.txt
+# echo 0 | sudo -S touch mnt/boot/config.txt
+# echo 0 | sudo -S echo 'zero:$6$nKazTU9UYkZ15LbS$aKazZgSp5z/ynlHPrDeaE7x5h4AF3bkioe0cJ02eGR7P75HEEOwaJGcunbh/bCzJipC/6UTT/Lh5KC8lhRaJP0' | sudo tee mnt/boot/userconf.txt
+# echo 0 | sudo -S echo 'arm_64bit=0' | sudo tee -a mnt/boot/config.txt
 echo 0 | sudo -S umount mnt/boot
 echo 0 | sudo -S umount mnt/root 2> /dev/null
 echo 0 | sudo -S mount -o loop,offset=537919488 ubuntu-22.04.5-preinstalled-server-armhf+raspi.img mnt/root
-echo 0 | sudo -S make -j6 O=$BASE_DIR/kernel/raspberrypi/armhf -C $BASE_DIR/raspberrypi-linux ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH=$BASE_DIR/mnt/root modules_install
+# echo 0 | sudo -S make -j6 O=$BASE_DIR/kernel/raspberrypi/armhf -C $BASE_DIR/raspberrypi-linux ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH=$BASE_DIR/mnt/root modules_install
 echo 0 | sudo -S umount mnt/root
 qemu-img resize -f raw ubuntu-22.04.5-preinstalled-server-armhf+raspi.img 8G
 
