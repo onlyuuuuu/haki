9012a2efcef19cabf91fc7076f4c591b4a49a9de
Andy Nguyen <andy.nguyen@championdata.com.au>
2024-11-29 22:49:32 +0700

    commit

--- src/main/ccpp/04/ex1.cpp
+++ src/main/ccpp/04/ex1.cpp
@@ -33,15 +33,21 @@ static int a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, x, y, z, w;
 //static multiset<int, decltype(compare_for_min_or_desc)>::iterator          index_dset_desc_it;
 static auto _ = []() { ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); return 0; }();
 static vector<vector<int>> movies;
+static auto ascn_mov_end_times = [] (int i1, int i2)
+{
+    if (movies[i1][1] != movies[i2][1])
+        return movies[i1][1] < movies[i2][1];
+    return i1 < i2;
+};
 static auto desc_mov_end_times = [] (int i1, int i2)
 {
-    if (movies[i1][1] != movies[i1][2])
-        return movies[i1][1] != movies[i1][2];
+    if (movies[i1][1] != movies[i2][1])
+        return movies[i1][1] > movies[i2][1];
     return i1 > i2;
 };
-static set<int, decltype(desc_mov_end_times)>           tree1(desc_mov_end_times);
+static set<int, decltype(ascn_mov_end_times)>           tree1(ascn_mov_end_times);
 static set<int, decltype(desc_mov_end_times)>           tree2(desc_mov_end_times);
-static set<int, decltype(desc_mov_end_times)>::iterator it1;
+static set<int, decltype(ascn_mov_end_times)>::iterator it1;
 static set<int, decltype(desc_mov_end_times)>::iterator it2;
 int main(int argc, char** argv)
 {
@@ -57,7 +63,6 @@ int main(int argc, char** argv)
         cout << n << endl;
         return 0;
     }
-    total = 0;
     for (i = 0; i < n; i++)
     {
         cin >> a;
@@ -66,7 +71,37 @@ int main(int argc, char** argv)
         movies.push_back(v);
         tree1.insert(i);
     }
-    
+    vector<int> init = { INT_MAX, 0 };
+    movies.push_back(init);
+    tree2.insert(*tree1.cbegin());
+    tree1.erase(tree1.cbegin());
+    total = 1;
+    while ((int)tree2.size() < k && !tree1.empty())
+    {
+        i = *tree1.cbegin();
+        tree1.erase(tree1.cbegin());
+        if (movies[*tree2.cbegin()][1] > movies[i][0])
+        {
+            tree2.insert(i);
+            ++total;
+            continue;
+        }
+        tree2.erase(tree2.cbegin());
+        tree2.insert(i);
+        ++total;
+    }
+    while (!tree1.empty())
+    {
+        i = *tree1.cbegin();
+        tree1.erase(tree1.cbegin());
+        movies[n][1] = movies[i][0];
+        it2 = tree2.lower_bound(n);
+        if (it2 == tree2.cend())
+            continue;
+        tree2.erase(it2);
+        tree2.insert(i);
+        ++total;
+    }
     cout << total << endl;
     return 0;
 }
