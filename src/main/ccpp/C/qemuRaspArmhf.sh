190beb8c6d1b6c17fe20b76d3e9da2d604d15aab
Andy <duyanhnn@outlook.com>
2024-11-30 23:33:50 +0700

    commit

--- src/main/ccpp/C/qemuRaspArmhf.sh
+++ src/main/ccpp/C/qemuRaspArmhf.sh
@@ -3,6 +3,7 @@
   # -netdev user,id=net0,hostfwd=tcp::2222-:22 \
   # -device usb-net,netdev=net0 \
 
+# From kernel
 echo 0 | sudo -S qemu-system-arm \
   -machine raspi2b \
   -drive if=sd,format=raw,file=2024-10-22-raspios-bookworm-armhf-lite.img \
@@ -10,6 +11,15 @@ echo 0 | sudo -S qemu-system-arm \
   -kernel kernel7.img \
   -append "rw earlyprintk loglevel=8 root=/dev/mmcblk0p2" #-no-reboot -nographic
 
+# From image
+echo 0 | sudo -S qemu-system-arm \
+  -machine raspi2b \
+  -drive if=sd,format=raw,file=2024-11-19-raspios-bookworm-armhf-lite.img \
+  -dtb bcm2709-rpi-2-b.dtb \
+  -kernel kernel7.img \
+  -append "rw earlyprintk loglevel=8 root=/dev/mmcblk0p2" #-no-reboot -nographic
+
+# From image (Ubuntu Server)
 echo 0 | sudo -S qemu-system-arm \
   -machine raspi2b \
   -drive if=sd,format=raw,file=ubuntu-22.04.5-preinstalled-server-armhf+raspi.img \
