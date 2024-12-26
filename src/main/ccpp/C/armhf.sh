3397652a68be482e44f45c834dfbc5abadd3022b
Andy <duyanhnn@outlook.com>
2024-12-26 21:50:13 +0700

    commit

--- src/main/ccpp/C/armhf.sh
+++ src/main/ccpp/C/armhf.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+echo 0 | sudo -S mkdir -p /usr/local/etc/qemu/
+echo 0 | sudo -S cp -rf bridge.conf /usr/local/etc/qemu/
 
+echo 0 | sudo -S mkdir -p /tools/qemu/bin/debug/native/qemu-bundle/usr/local/etc/qemu/
+echo 0 | sudo -S cp -rf bridge.conf /tools/qemu/bin/debug/native/qemu-bundle/usr/local/etc/qemu/
 
 echo 0 | sudo -S cp -rf qemu-ifup /etc/
 echo 0 | sudo -S cp -rf qemu-ifdown /etc/
+
+echo 0 | sudo -S mkdir -p /usr/local/etc/
+echo 0 | sudo -S cp -rf qemu-ifup /usr/local/etc/
+echo 0 | sudo -S cp -rf qemu-ifdown /usr/local/etc/
 
 mkdir -p /tools/qemu/bin/debug/native/qemu-bundle/usr/local/etc/
 cp -rf qemu-ifup /tools/qemu/bin/debug/native/qemu-bundle/usr/local/etc/
@@ -13,20 +21,36 @@ mkdir -p /tools/qemu/build/qemu-bundle/usr/local/etc/
 cp -rf qemu-ifup /tools/qemu/build/qemu-bundle/usr/local/etc/
 cp -rf qemu-ifdown /tools/qemu/build/qemu-bundle/usr/local/etc/
 
-/etc/qemu-ifup tap1 br1
-/etc/qemu-ifup tap2 br2
+echo 0 | sudo -S chmod -R 777 /dev/net/tun
+echo 0 | sudo -S chown -R zero:zero /dev/net/tun
 
-qemu-system-arm \
+/etc/qemu-ifup tap1 br1 ens38
+/etc/qemu-ifup tap2 br2 ens39
+
+echo 0 | sudo -S echo "Gained root access... Starting up VM now..."
+# sudo qemu-system-arm \
+#   -machine virt -cpu cortex-a15 -smp 8 -m 8192 \
+#   -kernel tools.dir/postnetinst-boot-vmlinuz-6.1.0-28-armmp-lpae -append 'root=/dev/vda2' \
+#   -initrd tools.dir/postnetinst-boot-initrd.img-6.1.0-28-armmp-lpae \
+#   -drive if=none,format=raw,file=tools.dir/hda.img,media=disk,id=hd0 \
+#   -device virtio-blk-device,drive=hd0 \
+#   -netdev tap,id=net0,ifname=tap0 \
+#   -device virtio-net-device,netdev=net0 \
+#   -netdev bridge,id=net1,br=br1 \
+#   -device virtio-net-device,netdev=net1 \
+#   -netdev user,id=net2 \
+#   -device virtio-net-device,netdev=net2 \
+#   -no-reboot \
+#   -monitor stdio
+sudo qemu-system-arm \
   -machine virt -cpu cortex-a15 -smp 8 -m 8192 \
-  -kernel tools.d/postnetinst-boot-vmlinuz-6.1.0-28-armmp-lpae -append 'root=/dev/vda2' \
-  -initrd tools.d/postnetinst-boot-initrd.img-6.1.0-28-armmp-lpae \
-  -drive if=none,format=raw,file=hda.img,media=disk,id=hd0 \
+  -kernel tools.dir/postnetinst-boot-vmlinuz-6.1.0-28-armmp-lpae -append 'root=/dev/vda2' \
+  -initrd tools.dir/postnetinst-boot-initrd.img-6.1.0-28-armmp-lpae \
+  -drive if=none,format=raw,file=tools.dir/hda.img,media=disk,id=hd0 \
   -device virtio-blk-device,drive=hd0 \
   -netdev tap,id=net0,ifname=tap0 \
-  -device virtio-net-device,netdev=net0 \
-  -netdev bridge,id=net1,br=br1 \
-  -device virtio-net-device,netdev=net1 \
-  -netdev user,id=net2 \
   -device virtio-net-device,netdev=net2 \
-  -no-reboot \
   -monitor stdio
+
+/etc/qemu-ifdown tap1 br1 ens38
+/etc/qemu-ifdown tap2 br2 ens39
--- src/main/ccpp/C/prepare.sh
+++ src/main/ccpp/C/prepare.sh
@@ -5,3 +5,27 @@
 # Get initrd and vmlinuz from install.ahf/ and use them to boot vm with qemu -kernel and initrd options
 # Install debian onto hda.img and make sure that our qemu NIC is backed by a internet-connected host interface/bridge
 # Make sure that we have internet connection to install Debian ARMHF (32 bit, ARMv7)
+
+# These commands are for libslirp (related to -net user)
+
+# Run this first as root: 
+# cd /tools/libslirp/ && meson build && ninja -C build install
+
+# Installing libslirp.so.0.4.0 to /usr/local/lib/x86_64-linux-gnu
+# Installing /tools/libslirp/src/libslirp.h to /usr/local/include/slirp
+# Installing /tools/libslirp/build/libslirp-version.h to /usr/local/include/slirp
+# Installing /tools/libslirp/build/meson-private/slirp.pc to /usr/local/lib/x86_64-linux-gnu/pkgconfig
+# Installing symlink pointing to libslirp.so.0.4.0 to /usr/local/lib/x86_64-linux-gnu/libslirp.so.0
+# Installing symlink pointing to libslirp.so.0 to /usr/local/lib/x86_64-linux-gnu/libslirp.so
+
+# mkdir -p /lib/x86_64-linux-gnu/OLD.libslirp.d/
+# mv /lib/x86_64-linux-gnu/libslirp.a /lib/x86_64-linux-gnu/OLD.libslirp.d/
+# mv /lib/x86_64-linux-gnu/libslirp.so /lib/x86_64-linux-gnu/OLD.libslirp.d/
+# mv /lib/x86_64-linux-gnu/libslirp.so.0 /lib/x86_64-linux-gnu/OLD.libslirp.d/
+# mv /lib/x86_64-linux-gnu/libslirp.so.0.4.0 /lib/x86_64-linux-gnu/OLD.libslirp.d/
+# tar czvf /lib/x86_64-linux-gnu/OLD.libslirp.d.tgz /lib/x86_64-linux-gnu/OLD.libslirp.d
+
+cp -rf /usr/local/lib/x86_64-linux-gnu/libslirp.so.0.4.0 /lib/x86_64-linux-gnu/
+cp -rf /usr/local/lib/x86_64-linux-gnu/pkgconfig/slirp.pc /lib/x86_64-linux-gnu/pkgconfig/
+cp -rf /usr/local/lib/x86_64-linux-gnu/libslirp.so /lib/x86_64-linux-gnu/
+cp -rf /usr/local/lib/x86_64-linux-gnu/libslirp.so.0 /lib/x86_64-linux-gnu/
