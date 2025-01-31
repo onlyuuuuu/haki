c9276c015d665689ae5c390dcce1381553846fcb
Andy <duyanhnn@outlook.com>
2025-01-31 13:46:58 +0700

    commit

--- src/main/ccpp/C/armhf.sh
+++ src/main/ccpp/C/armhf.sh
@@ -24,12 +24,13 @@ cp -rf qemu-ifdown /tools/qemu/build/qemu-bundle/usr/local/etc/
 echo 0 | sudo -S chmod -R 777 /dev/net/tun
 echo 0 | sudo -S chown -R zero:zero /dev/net/tun
 
-/etc/qemu-ifdown tap1 br1 ens38
-/etc/qemu-ifdown tap2 br2 ens39
-/etc/qemu-ifup tap1 br1 ens38
-/etc/qemu-ifup tap2 br2 ens39
+# /etc/qemu-ifdown tap1 br1 ens38
+# /etc/qemu-ifdown tap2 br2 ens39
+# /etc/qemu-ifup tap1 br1 ens38
+# /etc/qemu-ifup tap2 br2 ens39
 
 echo 0 | sudo -S echo "Gained root access... Starting up VM now..."
+
 # echo 0 | sudo -S qemu-system-arm \
 #   -machine virt -cpu cortex-a15 -smp 8 -m 8192 \
 #   -kernel tools.dir/postnetinst-boot-vmlinuz-6.1.0-28-armmp-lpae -append 'root=/dev/vda2' \
@@ -44,6 +45,7 @@ echo 0 | sudo -S echo "Gained root access... Starting up VM now..."
 #   -device virtio-net-device,netdev=net2 \
 #   -no-reboot \
 #   -monitor stdio
+
 echo 0 | sudo -S qemu-system-arm \
   -machine virt -cpu cortex-a15 -smp 8 -m 8192 \
   -kernel tools.dir/postnetinst-boot-vmlinuz-6.1.0-28-armmp-lpae -append 'root=/dev/vda2' \
