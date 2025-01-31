a41a8e7ca6c0790719d222775f52eb0a7529a07e
Andy <duyanhnn@outlook.com>
2025-01-31 13:35:19 +0700

    commit

--- src/main/ccpp/C/armhf.sh
+++ src/main/ccpp/C/armhf.sh
@@ -24,11 +24,13 @@ cp -rf qemu-ifdown /tools/qemu/build/qemu-bundle/usr/local/etc/
 echo 0 | sudo -S chmod -R 777 /dev/net/tun
 echo 0 | sudo -S chown -R zero:zero /dev/net/tun
 
+/etc/qemu-ifdown tap1 br1 ens38
+/etc/qemu-ifdown tap2 br2 ens39
 /etc/qemu-ifup tap1 br1 ens38
 /etc/qemu-ifup tap2 br2 ens39
 
 echo 0 | sudo -S echo "Gained root access... Starting up VM now..."
-# sudo qemu-system-arm \
+# echo 0 | sudo -S qemu-system-arm \
 #   -machine virt -cpu cortex-a15 -smp 8 -m 8192 \
 #   -kernel tools.dir/postnetinst-boot-vmlinuz-6.1.0-28-armmp-lpae -append 'root=/dev/vda2' \
 #   -initrd tools.dir/postnetinst-boot-initrd.img-6.1.0-28-armmp-lpae \
@@ -42,15 +44,12 @@ echo 0 | sudo -S echo "Gained root access... Starting up VM now..."
 #   -device virtio-net-device,netdev=net2 \
 #   -no-reboot \
 #   -monitor stdio
-sudo qemu-system-arm \
+echo 0 | sudo -S qemu-system-arm \
   -machine virt -cpu cortex-a15 -smp 8 -m 8192 \
   -kernel tools.dir/postnetinst-boot-vmlinuz-6.1.0-28-armmp-lpae -append 'root=/dev/vda2' \
   -initrd tools.dir/postnetinst-boot-initrd.img-6.1.0-28-armmp-lpae \
   -drive if=none,format=raw,file=tools.dir/hda.img,media=disk,id=hd0 \
   -device virtio-blk-device,drive=hd0 \
   -netdev tap,id=net0,ifname=tap0 \
-  -device virtio-net-device,netdev=net2 \
+  -device virtio-net-device,netdev=net0 \
   -monitor stdio
-
-/etc/qemu-ifdown tap1 br1 ens38
-/etc/qemu-ifdown tap2 br2 ens39
