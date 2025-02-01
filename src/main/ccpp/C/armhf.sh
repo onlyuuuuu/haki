55c2a0b85909dece02fb29a1ca043d6e82bff022
Andy <duyanhnn@outlook.com>
2025-02-01 16:03:27 +0700

    commit

--- src/main/ccpp/C/armhf.sh
+++ src/main/ccpp/C/armhf.sh
@@ -24,15 +24,10 @@ cp -rf qemu-ifdown /tools/qemu/build/qemu-bundle/usr/local/etc/
 echo 0 | sudo -S chmod -R 777 /dev/net/tun
 echo 0 | sudo -S chown -R zero:zero /dev/net/tun
 
-# /etc/qemu-ifdown tap1 br1 ens38
-# /etc/qemu-ifdown tap2 br2 ens39
-# /etc/qemu-ifup tap1 br1 ens38
-# /etc/qemu-ifup tap2 br2 ens39
-
 echo 0 | sudo -S echo "Gained root access... Starting up VM now..."
 
 # echo 0 | sudo -S qemu-system-arm \
-#   -machine virt -cpu cortex-a15 -smp 8 -m 8192 \
+#   -machine virt -cpu cortex-a15 -smp ${nproc} -m $((32*1024)) \
 #   -kernel tools.dir/vmlinuz -append 'root=/dev/vda2' \
 #   -initrd tools.dir/initrd.img \
 #   -drive if=none,format=raw,file=tools.dir/hda.img,media=disk,id=hd0 \
@@ -47,7 +42,7 @@ echo 0 | sudo -S echo "Gained root access... Starting up VM now..."
 #   -monitor stdio
 
 echo 0 | sudo -S qemu-system-arm \
-  -machine virt -cpu cortex-a15 -smp 8 -m 8192 \
+  -machine virt -cpu cortex-a15 -smp ${nproc} -m $((32*1024)) \
   -kernel tools.dir/vmlinuz -append 'root=/dev/vda2' \
   -initrd tools.dir/initrd.img \
   -drive if=none,format=raw,file=tools.dir/hda.img,media=disk,id=hd0 \
