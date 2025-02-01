5ff5ba2a61d5dc6605539d0e0574f51267adf062
Andy <duyanhnn@outlook.com>
2025-02-01 15:14:56 +0700

    commit

--- src/main/ccpp/C/test.c
+++ src/main/ccpp/C/test.c
@@ -3,12 +3,12 @@
 int main()
 {
     printf("\nHello there!\n\n");
-    printf("If you are seeing this output, congrats...\n");
-    printf("Use these commands to compile me:\n");
+    printf("If you are seeing this output, congrats...\n\n");
+    printf("Use these commands to compile me:\n\n");
     printf("  * On Linux (compile for OS-native target):\n");
-    printf("      $ gcc test.c -o test -g -Og\n");
+    printf("      $ gcc test.c -o test -g -Og\n\n");
     printf("  * On Linux (compile for ARM 32-bit target):\n");
     printf("      $ arm-linux-gnueabihf-gcc -Wl,-I/usr/arm-linux-gnueabihf/lib/ld-linux-armhf.so.3 test.c -o test -g -O0 -L/usr/arm-linux-gnueabihf/lib\n");
-    printf("      $ patchelf --set-rpath /usr/arm-linux-gnueabihf/lib test\n");
+    printf("      $ patchelf --set-rpath /usr/arm-linux-gnueabihf/lib test\n\n");
     return 0;
 }
