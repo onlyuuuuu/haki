3c160412eff70a458b80e3c51e198346ba286a46
Andy <duyanhnn@outlook.com>
2025-02-10 04:32:39 -0800

    commit

--- src/main/ccpp/C/compile.sh
+++ src/main/ccpp/C/compile.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-if [[ "$(uname -a)" == *"Linux"*"armv7l" ]]; then
+if [[ "$(uname -a)" == *"Linux"*"armv7l"* ]]; then
     gcc test.c -o test -g -Og
 else
     arm-linux-gnueabihf-gcc -Wl,-I/usr/arm-linux-gnueabihf/lib/ld-linux-armhf.so.3 test.c -o test -g -Og -L/usr/arm-linux-gnueabihf/lib
