7768bd7149e76baf042e10a622a98644894f0910
Andy <duyanhnn@outlook.com>
2025-04-01 20:43:31 +0700

    commit

---
+++ scripts/brew.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if command -v brew > /dev/null 2>&1; then
+  echo "Brew already installed, skipping further operations..."
+else
+  echo "Installing Brew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo >> /home/zero/.zshrc
+  echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/zero/.zshrc
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
+
+if grep 'brew' ~/.zshrc; then
+  echo "Brew seems to be sourced, no further actions..."
+else
+  echo "Sourcing brew by writing into ~/.zshrc now..."
+  echo >> /home/zero/.zshrc
+  echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/zero/.zshrc
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
+
+brew update
+brew upgrade
+brew cleanup
+
+brew install tlrc
+
