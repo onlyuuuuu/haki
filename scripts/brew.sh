7f8f08c75e8567c6ed8da53766386bbae0662728
Andy <duyanhnn@outlook.com>
2025-04-02 19:23:55 +0700

    commit

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -5,8 +5,8 @@ if command -v brew > /dev/null 2>&1; then
 else
   echo "Installing Brew"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  echo >> /home/zero/.zshrc
-  echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/zero/.zshrc
+  echo >> ~/.zshrc
+  echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.zshrc
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 
@@ -14,8 +14,8 @@ if grep 'brew' ~/.zshrc; then
   echo "Brew seems to be sourced, no further actions..."
 else
   echo "Sourcing brew by writing into ~/.zshrc now..."
-  echo >> /home/zero/.zshrc
-  echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/zero/.zshrc
+  echo >> ~/.zshrc
+  echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.zshrc
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 
