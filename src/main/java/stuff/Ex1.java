d17db8b7b598cb83d92fb70c7cf8d5ee10e30d6b
Andy <duyanhnn@outlook.com>
2025-05-10 10:52:33 +0700

    commit

--- src/main/java/stuff/Ex1.java
+++ src/main/java/stuff/Ex1.java
@@ -4,10 +4,31 @@ import java.util.*;
 
 public class Ex1
 {
-    public static int minimumLoss(List<Long> price)
+    public static int minimumLoss(List<Long> prices)
     {
         long minLoss = Long.MAX_VALUE;
-        TreeSet<Long> prices = new TreeSet<>();
+
+        TreeSet<Integer> descTree
+            = new TreeSet<>(Comparator.comparing(i -> prices.get(i)));
+
+        for (int i = 0; i < prices.size(); i++)
+            descTree.add(i);
+
+        for (int i = 0; i < prices.size() - 1; i++)
+        {
+            descTree.remove(i);
+            long buy = prices.get(i);
+
+            Integer floor = descTree.floor(i);
+            if (floor == null)
+                continue;
+
+            long sell = prices.get(floor);
+            long loss = buy - sell;
+            if (loss < minLoss)
+                minLoss = loss;
+        }
+
         return (int)minLoss;
     }
 }
--- src/test/java/stuff/Ex1Test.java
+++ src/test/java/stuff/Ex1Test.java
@@ -2,18 +2,39 @@ package stuff;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Ex1Test
 {
     @Test
+    public void testTree()
+    {
+        TreeSet<Integer> tree = new TreeSet<>(Comparator.reverseOrder());
+        tree.add(11);
+        tree.add(7);
+        tree.add(9);
+        tree.add(2);
+        // 11 9 7 2
+        // ceiling means right branch (>=, relative to order)
+        assertEquals(2, tree.ceiling(6));
+        assertEquals(7, tree.ceiling(7));
+        // floor means left branch (<=, relative to order)
+        assertEquals(9, tree.floor(9));
+        assertEquals(11, tree.floor(10));
+    }
+
+    @Test
     public void test()
     {
+        assertEquals(2, Ex1.minimumLoss(Arrays.asList(5l, 8l, 3l)));
+        assertEquals(2, Ex1.minimumLoss(Arrays.asList(20l, 7l, 8l, 2l, 5l)));
+        assertEquals(3, Ex1.minimumLoss(Arrays.asList(20l, 15l, 8l, 2l, 12l)));
         assertEquals(1, Ex1.minimumLoss(Arrays.asList(17l, 20l, 16l, 1l, 6l)));
-        assertEquals(1, Ex1.minimumLoss(Arrays.asList(-17l, -20l, -16l, -1l, -6l)));
-        assertEquals(3, Ex1.minimumLoss(Arrays.asList(1l, -5l, -2l, 10l, 20l)));
-        assertEquals(1, Ex1.minimumLoss(Arrays.asList(2l, 3l, 4l)));
+        assertEquals(3, Ex1.minimumLoss(Arrays.asList(1l, 5l, 2l, 10l, 20l)));
+        assertEquals(1, Ex1.minimumLoss(Arrays.asList(3l, 5l, 2l, 10l, 20l)));
+        assertEquals(1, Ex1.minimumLoss(Arrays.asList(4l, 3l, 5l)));
+        assertEquals(1, Ex1.minimumLoss(Arrays.asList(3l, 2l)));
     }
 }
