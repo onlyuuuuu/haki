6b18f665764681d2dba4afcf8e131787eb643413
Andy <duyanhnn@outlook.com>
2025-07-12 19:34:28 +0700

    Add in an option to do a normal compile

--- src/main/ccpp/C/compile.sh
+++ src/main/ccpp/C/compile.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-if [[ "$(uname -a)" == *"Linux"*"armv7l"* ]]; then
+if [[ "$(uname -a)" == *"Linux"*"armv7l"* || "$1" == "--just-do-it" || "$1" == "--normal"* ]]; then
     gcc test.c -o test -g -Og
 else
     arm-linux-gnueabihf-gcc -Wl,-I/usr/arm-linux-gnueabihf/lib/ld-linux-armhf.so.3 test.c -o test -g -Og -L/usr/arm-linux-gnueabihf/lib
