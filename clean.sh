259afeee0559dd7da7d3408899f79ee0ec581fc5
Andy <duyanhnn@outlook.com>
2025-07-31 22:41:29 +0700

    commit

--- clean.sh
+++ clean.sh
@@ -4,4 +4,4 @@ find . -type f -name "*.swp" -print -exec rm -f {} \;
 find . -type f -name "*.swo" -print -exec rm -f {} \;
 find ~/.local/state/nvim/swap/ -type f -name "*.swp" -print -exec rm -f {} \;
 find ~/.local/state/nvim/swap/ -type f -name "*.swo" -print -exec rm -f {} \;
-
+find . -type f -exec dos2unix {} \;
