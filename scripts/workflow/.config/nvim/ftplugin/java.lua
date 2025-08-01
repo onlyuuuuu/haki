0426f8821356d280ceaf31d46b7dbb51dabcec86
Andy <duyanhnn@outlook.com>
2025-08-01 20:56:14 +0700

    commit

--- scripts/workflow/.config/nvim/ftplugin/java.lua
+++ scripts/workflow/.config/nvim/ftplugin/java.lua
@@ -1,6 +1,12 @@
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+local workspace_dir = '/tools/java-lang-server-data/' .. project_name
 local config = 
 {
-  cmd = {'/tools/eclipse-java-ls/bin/jdtls'},
-  root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
+  cmd = 
+  {
+    '/tools/eclipse-java-ls/bin/jdtls',
+    '-data', workspace_dir
+  },
+  root_dir = vim.fs.dirname(vim.fs.find({'gradlew', 'gradlew.bat', '.git', 'mvnw', 'mvnw.bat'}, { upward = true })[1]),
 }
 require('jdtls').start_or_attach(config)
---
+++ src/main/java/TestLSP.java
@@ -0,0 +1,9 @@
+public class TestLSP
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World!");
+    return;
+  }
+}
+
