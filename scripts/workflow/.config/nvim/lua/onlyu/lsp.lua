ac6d4ee9714b3dc305a974644927436a91e24901
Andy <duyanhnn@outlook.com>
2025-08-19 19:13:56 +0700

    commit

--- scripts/workflow/.config/nvim/lua/onlyu/lsp.lua
+++ scripts/workflow/.config/nvim/lua/onlyu/lsp.lua
@@ -1,2 +1,8 @@
 -- Additional LSP configs
-
+local lspconfig = require('lspconfig')
+lspconfig.clangd.setup({
+  cmd = {'clangd', '--background-index', '--clang-tidy', '--log=verbose'},
+  init_options = {
+    fallbackFlags = { '-std=c++23' },
+  },
+})
