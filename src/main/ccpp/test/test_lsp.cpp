5b629e2cdc2d896f494b2d21818f481791fb26a0
Andy <duyanhnn@outlook.com>
2025-08-20 11:58:48 +0700

    commit

--- src/main/ccpp/test/test_lsp.cpp
+++ src/main/ccpp/test/test_lsp.cpp
@@ -5,6 +5,10 @@ using namespace std;
 int main()
 {
     cout << "Testing LSP, 1, 2, 3..." << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "Counting " << (i + 1) << "..." << endl;
+    }
     return 0;
 }
 
