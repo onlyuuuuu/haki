397fb60e5627d1f18c74fb4c217aa78c6bfee3bc
Andy <duyanhnn@outlook.com>
2025-08-26 21:30:42 +0700

    commit

---
+++ src/main/ccpp/vim/02/m1_s2_1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namepsace std;
+int main()
+{
+    stack<int> s;
+    string text;
+    cin >> text;
+    for (char c : text)
+    {
+        switch (c)
+        {
+            case 'C' -> s.push(12);
+        }
+    }
+    return 0;
+}
