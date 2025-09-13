86ac85d3ede3f4e5e1a838171fc2df0c5d6a055d
Andy <duyanhnn@outlook.com>
2025-09-13 09:40:41 +0700

    commit

--- src/main/ccpp/main/03/m1_s3_2.cpp
+++ src/main/ccpp/main/03/m1_s3_2.cpp
@@ -2,14 +2,12 @@
 using namespace std;
 int main()
 {
-    //ios_base::sync_with_stdio(false);
-    //cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int T, N, k, a;
-    // vector<vector<int>> r;
     cin >> T;
     for (int t = 1; t <= T; t++)
     {
-        // vector<int> v;
         multiset<int> ms;
         queue<int> q;
         cin >> N >> k;
@@ -20,25 +18,16 @@ int main()
             ms.insert(a);
         }
         cout << *ms.begin();
-        // v.push_back(*ms.begin());
         for (int i = k + 1; i <= N; i++)
         {
             cin >> a;
             q.push(a);
-            ms.erase(q.front());
+            ms.erase(ms.find(q.front()));
             ms.insert(a);
             q.pop();
             cout << " " << *ms.begin();
-            // v.push_back(*ms.begin());
         }
         cout << endl;
     }
-    // for (int i = 0; i < (int)r.size(); i++)
-    // {
-    //     cout << r[i][0];
-    //     for (int j = 1; j < (int)r[i].size(); j++)
-    //         cout << " " << r[i][j];
-    //     cout << endl;
-    // }
     return 0;
 }
