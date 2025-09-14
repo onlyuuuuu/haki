c0edd653855892ab9db3c070c6cd975a1366f550
Andy <duyanhnn@outlook.com>
2025-09-14 11:33:39 +0700

    commit

--- src/main/ccpp/main/03/m1_s3_3.cpp
+++ src/main/ccpp/main/03/m1_s3_3.cpp
@@ -5,7 +5,22 @@ public:
     Solution() {}
     int findMaxValueOfEquation(vector<vector<int>>& points, int k)
     {
-        
+        int mx = INT_MIN; int s1 = 0; deque<pair<int, int>> q;
+        q.push_back(make_pair(points[0][0], points[0][1] - points[0][0]));
+        for (int i = 1; i < points.size(); i++)
+        {
+            while (!q.empty() && points[i][0] - q.front().first > k) q.pop_front();
+            if (q.empty())
+            {
+                q.push_back(make_pair(points[i][0], points[i][1] - points[i][0]));
+                continue;
+            }
+            mx = max(mx, points[i][1] + points[i][0] + q.front().second);
+            s1 = points[i][1] - points[i][0];
+            while (!q.empty() && q.back().second <= s1) q.pop_back();
+            q.push_back(make_pair(points[i][0], s1));
+        }
+        return mx;
     }
 };
 int main()
