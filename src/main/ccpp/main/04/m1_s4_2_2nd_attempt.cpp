a01682753b94604ed203299276ee1281a2e79598
Andy <duyanhnn@outlook.com>
2025-09-17 22:08:39 +0700

    commit

--- src/main/ccpp/main/04/m1_s4_2_2nd_attempt.cpp
+++ src/main/ccpp/main/04/m1_s4_2_2nd_attempt.cpp
@@ -4,6 +4,16 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
+    int M,N,K,i,s1,s2;priority_queue<int,vector<int>,greater<int>>qa,qb;vector<int>vfa,vfb;
+    cin>>M>>N>>K;
+    cin>>i;vfb.push_back(i);M--;
+    for (;M>0;M--){cin>>i;qa.push(i);};
+    cin>>i;vfa.push_back(i);N--;
+    for (;N>0;N--){cin>>i;qb.push(i);};
+    cout<<vfa[0]+vfb[0]<<endl;
+    while (!qa.empty()&&!qb.empty())
+    {
+        
+    }
     return 0;
 }
