26506302f65befc60242ac3ca7cabe8c78aca739
Andy <duyanhnn@outlook.com>
2025-09-18 23:58:03 +0700

    commit

--- src/main/ccpp/main/04/m1_s4_2_2nd_attempt.cpp
+++ src/main/ccpp/main/04/m1_s4_2_2nd_attempt.cpp
@@ -2,26 +2,13 @@
 using namespace std;
 int main()
 {
-    int M,N,K,a,b;vector<int>va,vb;multiset<int>sa,sb,out;multiset<int>::iterator ita,itb,itout;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int M,N,K,a,b;vector<int>va,vb;multiset<int>sa,sb,sout;multiset<int>::iterator ita,itb,itout;
     cin>>M>>N>>K;
     for (;M>0;M--){cin>>a;sa.insert(a);}
     for (;N>0;N--){cin>>b;sb.insert(b);}
-    ita=sa.begin();va.push_back(*ita);ita++;
-    itb=sb.begin();vb.push_back(*itb);itb++;
-    while (ita!=sa.end()&&itb!=sb.end())
-    {
-        if (*ita >= *itb)
-        {
-            va.push_back(*ita);
-            ita++;
-        }
-        else
-        {
-            vb.push_back(*itb);
-            itb++;
-        }
-        if ((int)va.size()*(int)vb.size() > K)break;
-    }
-    while (ita!=sa.end()&&itb!=sb.end())
+    cout<<*sa.begin()+*sb.begin()<<endl;
+
     return 0;
 }
