aaf45d8b761b5c752a616b78c4bf98c7f8d8c016
Andy <duyanhnn@outlook.com>
2025-09-20 10:51:49 +0700

    commit

--- src/main/ccpp/main/04/m1_s4_2_2nd_attempt.cpp
+++ src/main/ccpp/main/04/m1_s4_2_2nd_attempt.cpp
@@ -4,10 +4,11 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int M,N,K,i,minAB;pair<int,bool>minp;multimap<int,bool>ma,mb;multimap<int,bool>::iterator mait,mbit;vector<int>va,vb;unordered_map<bool,vector<int>>mab;multiset<int>out;
+    int M,N,K,i,minAB,temp;pair<int,bool>minp;multimap<int,bool>ma,mb;multimap<int,bool>::iterator mait,mbit;
+    unordered_map<bool,vector<int>>mab;multiset<int>out;multiset<int>::iterator outit;
     cin>>M>>N>>K;for(;M>0;M--){cin>>i;ma.insert({i,false});}for(;N>0;N--){cin>>i;mb.insert({i,true});}
     minAB=min(ma.begin()->first,mb.begin()->first);out.insert(ma.begin()->first+mb.begin()->first);cout<<*out.begin()<<endl;
-    va.push_back(ma.begin()->first);vb.push_back(mb.begin()->first);mab[false]=va;mab[true]=vb;mait=++ma.begin();mbit=++mb.begin();
+    vector<int>va(M),vb(N);va.push_back(ma.begin()->first);vb.push_back(mb.begin()->first);mab[false]=va;mab[true]=vb;mait=++ma.begin();mbit=++mb.begin();
     for (minp=min(*mait,*mbit);mait!=ma.end()&&mbit!=mb.end()&&out.size()!=K;minp=min(*mait,*mbit))
     {
         for (const auto& e : mab[!minp.second])
@@ -18,14 +19,36 @@ int main()
         mab[minp.second].push_back(minp.first);
         if (!minp.second) ++mait; else ++mbit;
     }
-    for (minp=min(*mait,*mbit);minAB+minp.first<*out.end();minp=min(*mait,*mbit))
+    for (;mait!=ma.end()&&out.size()!=K;++mait)
+    {
+        for (const auto& e : mab[true])
+        {
+            if (out.size()==K)break;
+            out.insert(e+mait->first);
+        }
+        mab[false].push_back(mait->first);
+    }
+    for (;mbit!=mb.end()&&out.size()!=K;++mbit)
+    {
+        for (const auto& e : mab[false])
+        {
+            if (out.size()==K)break;
+            out.insert(e+mbit->first);
+        }
+        mab[true].push_back(mbit->first);
+    }
+    for (minp=min(*mait,*mbit);minAB+minp.first<*out.rbegin();minp=min(*mait,*mbit))
     {
         for (const auto& e : mab[!minp.second])
         {
-            if (minAB+minp.first>=*out.end())break;
-            out.erase(out.end());
-            out.insert(e+minp.first);
+            temp=e+minp.first;
+            if (temp>=*out.rbegin())break;
+            out.erase(--out.end());
+            out.insert(temp);
         }
+        mab[minp.second].push_back(minp.first);
+        if (!minp.second) ++mait; else ++mbit;
     }
+    for (outit=++out.begin();outit!=out.end();++outit)cout<<*outit<<endl;
     return 0;
 }
