18e69f38e2309e965de208470bacaa53523e0a99
Andy <duyanhnn@outlook.com>
2025-09-21 14:21:57 +0700

    commit

--- src/main/ccpp/main/04/m1_s4_2_4th_attempt.cpp
+++ src/main/ccpp/main/04/m1_s4_2_4th_attempt.cpp
@@ -4,6 +4,9 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int M,N,K,i;
+    int M,N,K,i;cin>>M>>N>>K;
+    priority_queue<pair<int,bool>,vector<pair<int,bool>>,greater<pair<int,bool>>>qa,qb;
+    vector<int>va,vb;va.reserve(M);vb.reserve(N);unordered_map<bool,vector<int>>m;m.reserve(2);m[false]=va;m[true]=vb;
+
     return 0;
 }
