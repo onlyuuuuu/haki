110516adb8d81ee628a4c4c5c1dca9591d05fb94
Andy <duyanhnn@outlook.com>
2025-09-21 20:35:27 +0700

    commit

---
+++ src/main/ccpp/main/04/m1_s4_2_7th_attempt.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int M,N,K,i;cin>>M>>N>>K;
+    multimap<int,bool>A,B;
+    multimap<int,bool>::const_iterator ita,itb;
+    vector<int>a,b;a.reserve(M);b.reserve(N);
+    unordered_map<bool,vector<int>>m;m.reserve(2);m[false]=a;m[true]=b;
+    multiset<int>out;multiset<int>::const_iterator it;
+    for (;M>0;M--){cin>>i;A.insert({i,false});}
+    for (;N>0;N--){cin>>i;B.insert({i,true});}
+    ita=A.cbegin();itb=B.cbegin();
+    m[false].push_back(ita->first);
+    m[true].push_back(itb->first);
+    out.insert(ita->first+itb->first);
+    for (;K>1;K--){out.insert(INT_MAX);}
+    ++ita;++itb;
+    pair<int,bool>minp={INT_MAX,true};
+    M=min(m[false][0],m[true][0]);
+    // ----------------------------------------
+    for (;ita!=A.cend()&&itb!=B.cend();)
+    {
+        minp=min(*ita,*itb);
+        if (M+minp.first>=*out.crbegin())break;
+        for (const auto &x : m[!minp.second])
+        {
+            i=x+minp.first;
+            if (i>=*out.crbegin())break;
+            out.erase(--out.end());
+            out.insert(i);
+        }
+        m[minp.second].push_back(minp.first);
+        if (!minp.second)++ita;else++itb;
+    }
+    for (;ita!=A.cend();++ita)
+    {
+        if (M+ita->first>=*out.crbegin())break;
+        for (const auto &x : m[!ita->second])
+        {
+            i=x+ita->first;
+            if (i>=*out.crbegin())break;
+            out.erase(--out.end());
+            out.insert(i);
+        }
+        m[ita->second].push_back(ita->first);
+    }
+    for (;itb!=B.cend();++itb)
+    {
+        if (M+itb->first>=*out.crbegin())break;
+        for (const auto &x : m[!itb->second])
+        {
+            i=x+itb->first;
+            if (i>=*out.crbegin())break;
+            out.erase(--out.end());
+            out.insert(i);
+        }
+        m[itb->second].push_back(itb->first);
+    }
+    // ----------------------------------------
+    for (it=out.cbegin();it!=out.cend();++it){cout<<*it<<endl;}
+    return 0;
+}
