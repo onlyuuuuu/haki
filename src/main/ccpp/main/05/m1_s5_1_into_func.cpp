24bf4573a8e5c4f007e12a96db141014c5cd7ed3
Andy <duyanhnn@outlook.com>
2025-09-21 12:52:17 +0700

    commit

---
+++ src/main/ccpp/main/05/m1_s5_1_into_func.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'minimumLoss' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts LONG_INTEGER_ARRAY price as parameter.
+ */
+
+int minimumLoss(vector<long> price)
+{
+    long i=0;map<long,long,greater<long>>m;
+    for (const auto&x:price){m[x]=++i;}
+    auto pre=m.cbegin();i=INT_MAX;
+    for (auto cur=++m.cbegin();cur!=m.cend();++cur)
+    {
+        if (pre->second<cur->second)
+            i=min(i,pre->first-cur->first);
+        ++pre;
+    }
+    return i;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string price_temp_temp;
+    getline(cin, price_temp_temp);
+
+    vector<string> price_temp = split(rtrim(price_temp_temp));
+
+    vector<long> price(n);
+
+    for (int i = 0; i < n; i++) {
+        long price_item = stol(price_temp[i]);
+
+        price[i] = price_item;
+    }
+
+    int result = minimumLoss(price);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
