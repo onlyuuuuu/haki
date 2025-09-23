007bdb64f681cc96c9fc307ee51a7aad0630fb52
Andy <duyanhnn@outlook.com>
2025-09-23 18:52:33 +0700

    commit

--- src/main/ccpp/main/05/m1_s5_2_5th_attempt.cpp
+++ src/main/ccpp/main/05/m1_s5_2_5th_attempt.cpp
@@ -5,7 +5,7 @@ struct comp
     bool operator()(const pair<int,int>& a,const pair<int,int>& b) const
     {
         if (a.second != b.second)
-            return a.second < b.second;
+            return a.second > b.second;
         return a.first > b.first;
     }
 };
@@ -13,13 +13,27 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int n,k,a,b;cin>>n>>k;if (n==k)cout<<n<<endl;return 0;
-    vector<pair<int,int>> v;v.reserve(n);
-    priority_queue<pair<int,int>, vector<pair<int,int>>, comp> q(comp(),move(v));
-    multimap<int,int>m;
+    int n,k,a,b;cin>>n>>k;if(n==k){cout<<n<<endl;return 0;}
+    vector<pair<int,int>>v;v.reserve(n);
+    priority_queue<pair<int,int>, vector<pair<int,int>>, comp>q(comp(),move(v));
+    multiset<int,greater<int>>s;multiset<int,greater<int>>::iterator it;
     for (;n>0;n--){cin>>a>>b;q.push({a,b});}
+    s.insert(q.top().second);q.pop();++n;
     // ------------------------------
-
+    for (;!q.empty();q.pop())
+    {
+        it=s.lower_bound(q.top().first);
+        if (it==s.end())
+        {
+            if ((int)s.size()==k) continue;
+            s.insert(q.top().second);
+            ++n;
+            continue;
+        }
+        s.erase(it);
+        s.insert(q.top().second);
+        ++n;
+    }
     // ------------------------------
     cout<<n<<endl;
     return 0;
