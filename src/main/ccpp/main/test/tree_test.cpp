e97595969db1c99004e56f536b97f93c29efbc78
Andy <duyanhnn@outlook.com>
2025-09-25 23:54:21 +0700

    commit

--- src/main/ccpp/main/test/tree_test.cpp
+++ src/main/ccpp/main/test/tree_test.cpp
@@ -1,39 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-// Comparator: sort by first, and then by second if first is equal
-struct compare_by_first
-{
-    bool operator()(const pair<int, int>& a, const pair<int, int>& b) const
-    {
-        if (a.first == b.first)
-            return a.second < b.second;
-        return a.first < b.first;
-    }
-};
-
 int main()
 {
-    set<pair<int,int>, compare_by_first> tree;
+    set<int>s;
+    s.insert(8);
+    s.insert(2);
+    s.insert(4);
+    s.insert(11);
 
-    tree.insert({3, 100});
-    tree.insert({1, 200});
-    tree.insert({2, 150});
-    tree.insert({3, 50});
-
-    // tree.cbegin()->first = -1;
-    // tree.cbegin()->second = 201;
-
-    for (const auto& p : tree)
-        cout << "(" << p.first << ", " << p.second << ")\n";
-
-    // auto b = tree.lower_bound(make_pair(2, 150));
-    // if (b == tree.cend())
-    // {
-    //     cout << "Searched: END/NIL" << endl;
-    //     return 0;
-    // }
-    // cout << "Searched: (" << b->first << ", " << b->second << ")\n";
+    cout<<*s.find(4)<<endl;
+    cout<<*--s.find(4)<<endl;
+    cout<<*++s.find(4)<<endl;
     
     return 0;
 }
