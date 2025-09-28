e86a4e4ab14678de039fdf627a303b7d86d0b539
Andy <duyanhnn@outlook.com>
2025-09-28 11:10:55 +0700

    commit

--- src/main/ccpp/main/05/m1_s5_3_2nd_attempt.cpp
+++ src/main/ccpp/main/05/m1_s5_3_2nd_attempt.cpp
@@ -4,7 +4,9 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    int n;cin>>n;string s;int i;cin>>s>>i;int m = i;
+    string s;double m=0;int x,n;cin>>n;
+    multiset<int,greater<>>t1;
+    multiset<int>t2;
     for (;n>1;n--)
     {
         cin>>s;
@@ -13,7 +15,29 @@ int main()
             cout<<m<<endl;
             continue;
         }
-        cin>>i;
+        cin>>x;
+        if (s=="IN")
+        {
+            if (x<=m)
+            {
+                if (x<=*t1.crbegin()) t1.insert(--t1.end(),x);
+                else if (x>=*t1.cbegin()) t1.insert(t1.begin(),x);
+                else t1.insert(x);
+
+            }
+            else
+            {
+                if (x>=*t2.crbegin()) t2.insert(--t2.end(),x);
+                else if (x<=*t2.cbegin()) t2.insert(t2.begin(),x);
+                else t2.insert(x);
+
+            }
+
+        }
+        else
+        {
+
+        }
     }
     return 0;
 }
