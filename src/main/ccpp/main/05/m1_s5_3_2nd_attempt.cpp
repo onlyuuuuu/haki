320f95bb6623222db3c13584dc790508044af5ef
Andy <duyanhnn@outlook.com>
2025-09-29 22:47:52 +0700

    commit

--- src/main/ccpp/main/05/m1_s5_3_2nd_attempt.cpp
+++ src/main/ccpp/main/05/m1_s5_3_2nd_attempt.cpp
@@ -18,25 +18,83 @@ int main()
         cin>>x;
         if (s=="IN")
         {
+            if (t1.empty()&&t2.empty())
+            {
+                t1.insert(x);
+                m=x;
+                continue;
+            }
             if (x<=m)
             {
-                if (x<=*t1.crbegin()) t1.insert(--t1.end(),x);
-                else if (x>=*t1.cbegin()) t1.insert(t1.begin(),x);
+                if (t1.empty())
+                {
+                    t1.insert(x);
+                    m=(*t1.begin()+*t2.begin())/2;
+                    continue;
+                }
+                if (x<=*t1.rbegin()) t1.insert(--t1.end(),x);
+                else if (x>=*t1.begin()) t1.insert(t1.begin(),x);
                 else t1.insert(x);
-
+                if ((int)t1.size()-(int)t2.size()==2)
+                {
+                    t2.insert(t2.begin(),*t1.begin());
+                    t1.erase(t1.begin());
+                }
+                else if ((int)t1.size()-(int)t2.size()==1)
+                    m=*t1.begin();
+                else
+                    m=(*t1.begin()+*t2.begin())/2;
             }
             else
             {
-                if (x>=*t2.crbegin()) t2.insert(--t2.end(),x);
-                else if (x<=*t2.cbegin()) t2.insert(t2.begin(),x);
+                if (t2.empty())
+                {
+                    t2.insert(x);
+                    m=(*t1.begin()+*t2.begin())/2;
+                    continue;
+                }
+                if (x>=*t2.rbegin()) t2.insert(--t2.end(),x);
+                else if (x<=*t2.begin()) t2.insert(t2.begin(),x);
                 else t2.insert(x);
-
+                if ((int)t2.size()-(int)t1.size()==2)
+                {
+                    t1.insert(t1.begin(),*t2.begin());
+                    t2.erase(t2.begin());
+                }
+                else if ((int)t2.size()-(int)t1.size()==1)
+                    m=*t2.begin();
+                else
+                    m=(*t1.begin()+*t2.begin())/2;
             }
-
         }
         else
         {
-
+            if (x<=m)
+            {
+                t1.erase(t1.find(x));
+                if ((int)t2.size()-(int)t1.size()==2)
+                {
+                    t1.insert(t1.begin(),*t2.begin());
+                    t2.erase(t2.begin());
+                }
+                else if ((int)t2.size()-(int)t1.size()==1)
+                    m=*t2.begin();
+                else
+                    m=(*t1.begin()+*t2.begin())/2;
+            }
+            else
+            {
+                t2.erase(t2.find(x));
+                if ((int)t1.size()-(int)t2.size()==2)
+                {
+                    t2.insert(t2.begin(),*t1.begin());
+                    t1.erase(t1.begin());
+                }
+                else if ((int)t1.size()-(int)t2.size()==1)
+                    m=*t1.begin();
+                else
+                    m=(*t1.begin()+*t2.begin())/2;
+            }
         }
     }
     return 0;
