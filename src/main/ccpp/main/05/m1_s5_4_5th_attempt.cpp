5c1318f5d46a50347d20347b4305f33367b50e93
Andy <duyanhnn@outlook.com>
2025-10-06 20:53:27 +0700

    commit

--- src/main/ccpp/main/05/m1_s5_4_5th_attempt.cpp
+++ src/main/ccpp/main/05/m1_s5_4_5th_attempt.cpp
@@ -1,68 +1,85 @@
 #include <bits/stdc++.h>
 using namespace std;
 static constexpr char char_a='a';
+static vector<string> dictionary;
 class input
 {
 public:
-    string text;int start;
+    int _id,_start;
     input(){}
-    input(string text,int start)
+    input(int id,int start):_id(id),_start(start){}
+    string get_text() const
     {
-        this->text=text;
-        this->start=start;
+        return dictionary[_id];
+    }
+    int length() const
+    {
+        return dictionary[_id].length();
+    }
+    void replace_longer_string(int id,int start)
+    {
+        if (dictionary[id].length()>dictionary[_id].length())
+        {
+            this->_id=id;
+            this->_start=start;
+        }
     }
 };
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    string result,t;int n,k,x,e;cin>>n;
+    string result,t;int n,k,x,e,i=-1;cin>>n;
     map<int,input>m;
-    map<int,input>::iterator i;
-    while (n--)
+    map<int,input>::iterator it;
+    pair<map<int,input>::iterator,bool>irp;
+    cin>>t>>k>>x;
+    dictionary.push_back(t);++i;
+    it=m.emplace(piecewise_construct,forward_as_tuple(x+static_cast<int>(t.length())),forward_as_tuple(i,x)).first;
+    while (--k)
     {
-        cin>>t>>k;
-        while (k--)
+        cin>>x;
+        it=m.emplace_hint(it,piecewise_construct,forward_as_tuple(x+static_cast<int>(t.length())),forward_as_tuple(i,x));
+    }
+    while (--n)
+    {
+        cin>>t>>k>>x;
+        dictionary.push_back(t);++i;
+        e=x+static_cast<int>(t.length());
+        it=m.emplace(piecewise_construct,forward_as_tuple(e),forward_as_tuple(i,x)).first;
+        it->second.replace_longer_string(i,x);
+        while (--k)
         {
             cin>>x;
             e=x+static_cast<int>(t.length());
-            i=m.find(e);
-            if (i==m.end())
-                m.emplace(piecewise_construct,forward_as_tuple(e),forward_as_tuple(t,x));
-            else
-            {
-                if (t.length()>i->second.text.length())
-                {
-                    i->second.text=t;
-                    i->second.start=x;
-                }
-            }
+            it=m.emplace_hint(it,piecewise_construct,forward_as_tuple(e),forward_as_tuple(i,x));
+            it->second.replace_longer_string(i,x);
         }
     }
-    if (m.rbegin()->second.start==1)
+    if (m.rbegin()->second._start==1)
     {
-        cout<<m.rbegin()->second.text;
+        cout<<m.rbegin()->second.get_text();
         return 0;
     }
-    n=m.rbegin()->second.start;
-    result=m.rbegin()->second.text;
+    n=m.rbegin()->second._start;
+    result=m.rbegin()->second.get_text();
     m.erase(--m.end());
-    for (i=m.lower_bound(n);!m.empty();i=m.lower_bound(n))
+    for (it=m.lower_bound(n);!m.empty();it=m.lower_bound(n))
     {
-        if (i->second.start==1)
+        if (it->second._start==1)
         {
-            cout<<((i->first==n)?i->second.text:i->second.text.substr(0,n-i->second.start))<<result;
+            cout<<((it->first==n)?it->second.get_text():it->second.get_text().substr(0,n-it->second._start))<<result;
             return 0;
         }
-        while (i!=m.end() && i->second.start>=n) i=m.erase(i);
-        if (i==m.end())
+        while (it!=m.end() && it->second._start>=n) it=m.erase(it);
+        if (it==m.end())
         {
-            --i;
-            for (k=n-i->first;k--;) result=char_a+result;
-            result=i->second.text+result;
+            --it;
+            for (k=n-it->first;k--;) result=char_a+result;
+            result=it->second.get_text()+result;
         }
-        else result=((i->first==n)?i->second.text:i->second.text.substr(0,n-i->second.start))+result;
-        n=i->second.start;
+        else result=((it->first==n)?it->second.get_text():it->second.get_text().substr(0,n-it->second._start))+result;
+        n=it->second._start;
     }
     for (k=n-1;k--;) cout<<char_a;
     cout<<result;
