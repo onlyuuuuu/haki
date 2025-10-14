fdd4b455718f75069c39c784a96f4132c33e0ddf
Andy <duyanhnn@outlook.com>
2025-10-14 22:11:13 +0700

    commit

--- src/main/ccpp/main/05/m1_s5_4_7th_attempt.cpp
+++ src/main/ccpp/main/05/m1_s5_4_7th_attempt.cpp
@@ -1,10 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-static constexpr char char_a='a';
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-
+    string t,s;int n,k,i;cin>>n;
+    while (n--)
+    {
+        cin>>t>>k>>i;
+    }
     return 0;
 }
