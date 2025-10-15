06e25f7077ec54e65bc5a7848eba8e649623d9e3
Andy <duyanhnn@outlook.com>
2025-10-15 15:16:17 +0700

    commit

--- src/main/ccpp/main/05/m1_s5_4_7th_attempt.cpp
+++ src/main/ccpp/main/05/m1_s5_4_7th_attempt.cpp
@@ -4,20 +4,22 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    string t,s;int n,k,i;cin>>n;
+    string t,s;int n,k,i,len,end=INT_MIN;cin>>n;
     map<int,map<int,string,greater<int>>,greater<int>>m;
     map<int,map<int,string,greater<int>>,greater<int>>::iterator it;
     map<int,string,greater<int>>::iterator h;
     while (n--)
     {
         cin>>t>>k>>i;
-        auto&sm=m.try_emplace(static_cast<int>(t.length()),map<int,string,greater<int>>()).first->second;
+        len=static_cast<int>(t.length());
+        auto&sm=m.try_emplace(len).first->second;
         h=sm.emplace(i,t).first;
         while (--k)
         {
             cin>>i;
             sm.emplace_hint(h,i,t);
         }
+        end=max(end,i+len);
     }
     return 0;
 }
