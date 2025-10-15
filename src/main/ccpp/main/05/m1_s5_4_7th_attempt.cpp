2b19e5f7aba4187820a98e7c1b2a3ce0f0461715
Andy <duyanhnn@outlook.com>
2025-10-15 21:10:59 +0700

    commit

--- src/main/ccpp/main/05/m1_s5_4_7th_attempt.cpp
+++ src/main/ccpp/main/05/m1_s5_4_7th_attempt.cpp
@@ -4,26 +4,36 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    string t,s="";int n,k,i,len,end=INT_MIN;cin>>n;
+    bool stop;string t,s="";int n,k,i,len,end=INT_MIN;cin>>n;
     map<int,map<int,string,greater<int>>,greater<int>>m;
-    map<int,string,greater<int>>::iterator it;
+    map<int,string,greater<int>>::iterator i1,found;
+    map<int,map<int,string,greater<int>>,greater<int>>::iterator i2,min;
     while (n--)
     {
         cin>>t>>k>>i;
         len=static_cast<int>(t.length());
         auto&sm=m.try_emplace(len).first->second;
-        it=sm.emplace(i,t).first;
+        i1=sm.emplace(i,t).first;
         while (--k)
         {
             cin>>i;
-            sm.emplace_hint(it,i,t);
+            sm.emplace_hint(i1,i,t);
         }
         end=max(end,i+len);
     }
-    len=end-1;
-    for (n=1;static_cast<int>(s.length()) != len;)
+    n=1;
+    for (;;)
     {
-
+        stop=false;
+        min=m.begin();
+        for (i2=m.begin();i2!=m.end();i2++)
+        {
+            if (i2->second.rbegin()->first==n)
+            {
+                stop=true;
+                n+=static_cast<int>(i2->second.rbegin()->second.length());
+            }
+        }
     }
     return 0;
 }
