9c7a4f25b9a8026e0c9343a9bfff805715aa7eba
Andy <duyanhnn@outlook.com>
2025-10-16 22:34:55 +0700

    commit

--- src/main/ccpp/main/05/m1_s5_4_8th_attempt.cpp
+++ src/main/ccpp/main/05/m1_s5_4_8th_attempt.cpp
@@ -1,29 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-class token
+struct token
 {
-public:
     int start,end;
     string text;
-    token(const int start,const int end,const string&text):start(start),end(end),text(text){}
+    token(const int start,const string&text):start(start),end(start+static_cast<int>(text.length())),text(text){}
 };
-class input
+struct token_compare
 {
-public:
-    int length;
-    map<int,token,greater<int>>tokens;
-    explicit input(const string&text):length(static_cast<int>(text.length())){}
-    explicit input(const size_t&size):length(static_cast<int>(size)){}
-    token min() const{return tokens.rbegin()->second;}
-    token max() const{return tokens.begin()->second;}
-    int min_start() const{return tokens.rbegin()->second.start;}
-    int max_start() const{return tokens.begin()->second.start;}
+    using is_transparent=void;
+    bool operator()(const token&a,const token&b)const{a.start>b.start;}
+    bool operator()(const int a,const token&b)const{a>b.start;}
+    bool operator()(const token&a,const int b)const{a.start>b;}
+};
+struct input
+{
+    set<token,token_compare>tokens;
+    input(){}
+    token min() const{return *tokens.rbegin();}
+    token max() const{return *tokens.begin();}
+    int min_start() const{return tokens.rbegin()->start;}
+    int max_start() const{return tokens.begin()->start;}
     const token* before(const int pos) const
     {
-        if (tokens.rbegin()->second.start > pos) return nullptr;
-        if (tokens.rbegin()->second.start == pos) return &tokens.rbegin()->second;
-        if (tokens.begin()->second.start <= pos) return &tokens.begin()->second;
-        return &tokens.lower_bound(pos)->second;
+        if (tokens.rbegin()->start > pos) return nullptr;
+        if (tokens.rbegin()->start == pos) return &*tokens.rbegin();
+        if (tokens.begin()->start <= pos) return &*tokens.begin();
+        return &*tokens.lower_bound(pos);
     }
 };
 int main()
@@ -32,5 +35,19 @@ int main()
     cin.tie(nullptr);
     map<int,input,greater<int>>m;
     map<int,input,greater<int>>::iterator it;
+    set<token,token_compare>::iterator h;
+    string s,t;int n,k,start,end;cin>>n;
+    while (n--)
+    {
+        cin>>t>>k>>start;
+        auto&tokens=m.try_emplace(static_cast<int>(t.length())).first->second.tokens;
+        h=tokens.emplace(start,t).first;
+        while (--k)
+        {
+            cin>>start;
+            h=tokens.emplace_hint(h,start,t);
+        }
+        end=max(end,h->end);
+    }
     return 0;
 }
