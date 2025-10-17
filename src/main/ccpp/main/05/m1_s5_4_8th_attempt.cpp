6d19ce3cb504d767840731a46f61114fd654b555
Andy <duyanhnn@outlook.com>
2025-10-17 12:33:38 +0700

    commit

--- src/main/ccpp/main/05/m1_s5_4_8th_attempt.cpp
+++ src/main/ccpp/main/05/m1_s5_4_8th_attempt.cpp
@@ -1,10 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
+struct token;
+struct token_compare;
+struct input;
 struct token
 {
     int start,end;
     string text;
-    token(const int start,const string&text):start(start),end(start+static_cast<int>(text.length())),text(text){}
+    input*owner;
+    token(int start,string&text,input* owner):start(start),end(start+static_cast<int>(text.length())),text(text),owner(owner){}
 };
 struct token_compare
 {
@@ -16,15 +20,33 @@ struct token_compare
 struct input
 {
     set<token,token_compare>tokens;
-    input(){}
-    token min() const{return *tokens.rbegin();}
-    int min_start() const{return tokens.rbegin()->start;}
-    void erase_min(){tokens.erase(--tokens.end());}
-    const token* before(const int pos) const
+    map<int,input,greater<int>>*owner;
+    input(map<int,input,greater<int>>*owner):owner(owner){}
+    token min(){return *tokens.rbegin();}
+    int min_start(){return tokens.rbegin()->start;}
+    void erase_min(map<int,input,greater<int>>::iterator& it)
+    {
+        tokens.erase(--tokens.end());
+        if (it->second.empty()) owner->erase(it);
+    }
+    bool empty(){return tokens.empty();}
+    const token* before_or_equals(int pos,map<int,input,greater<int>>::iterator& it)
     {
         if (tokens.rbegin()->start > pos) return nullptr;
         if (tokens.rbegin()->start == pos) return &*tokens.rbegin();
-        if (tokens.begin()->start <= pos) return &*tokens.begin();
+        if (tokens.begin()->start == pos)
+        {
+            owner->erase(it);
+            return &*tokens.begin();
+        }
+        if (tokens.begin()->start < pos)
+        {
+            if (tokens.begin()->end > pos)
+            {
+                owner->erase(it);
+                return &*tokens.begin();
+            }
+        }
         return &*tokens.lower_bound(pos);
     }
 };
@@ -40,12 +62,13 @@ int main()
     while (n--)
     {
         cin>>t>>k>>start;
-        auto&tokens=m.try_emplace(static_cast<int>(t.length())).first->second.tokens;
-        h=tokens.emplace(start,t).first;
+        auto&input=m.try_emplace(static_cast<int>(t.length()),&m).first->second;
+        auto&tokens=input.tokens;
+        h=tokens.emplace(start,t,&input).first;
         while (--k)
         {
             cin>>start;
-            h=tokens.emplace_hint(h,start,t);
+            h=tokens.emplace_hint(h,start,t,&input);
         }
         end=max(end,h->end);
     }
@@ -54,8 +77,8 @@ int main()
         closest=m.begin();
         for (it=m.begin();it!=m.end();it++)
         {
-            found=it->second.before(start);
-            if (found) break;
+            found=it->second.before_or_equals(start,it);
+            if (found && found->end > start) break; // fix
             if (it->second.min_start() < closest->second.min_start()) closest=it;
         }
         if (!found)
@@ -64,7 +87,22 @@ int main()
             while (n--) s+='a';
             s+=closest->second.min().text;
             start=closest->second.min().end;
-            closest->second.tokens.erase(closest->second.min());
+            closest->second.erase_min(closest);
+            continue;
+        }
+        if (found->start==start)
+        {
+            s+=found->text;
+            start=found->end;
+            it->second.tokens.erase(found->start);
+            if (it->second.empty()) m.erase(it);
+            continue;
+        }
+        n=found->end;
+        for (++it;it!=m.end();it++)
+        {
+            if (start + it->first <= n) break;
+            found=it->second.before_or_equals(start,it);
         }
     }
     return 0;
