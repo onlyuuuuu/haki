567d84fe1e662d16dd3872b8b1d997211649fb9e
Andy <duyanhnn@outlook.com>
2025-10-17 23:07:53 +0700

    commit

--- src/main/ccpp/main/05/m1_s5_4_8th_attempt.cpp
+++ src/main/ccpp/main/05/m1_s5_4_8th_attempt.cpp
@@ -14,16 +14,26 @@ struct input
     map<int,input,greater<int>>*inputs;
     input(map<int,input,greater<int>>*inputs):inputs(inputs){}
 };
+map<int,token,greater<int>>::iterator min_start_off_range(map<int,token,greater<int>>::iterator a, map<int,token,greater<int>>::iterator b)
+{
+    if (a->first < b->first) return a;
+    return b;
+}
+token* max_end_in_range(token& a, token& b)
+{
+    if (a.end > b.end) return &a;
+    return &b;
+}
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     map<int,input,greater<int>>m;
     map<int,input,greater<int>>::iterator it;
-    map<int,token,greater<int>>::iterator h;
+    map<int,token,greater<int>>::iterator h,next,found;
     string s,t;int n,k,start,end;cin>>n;
-    bool stop=false;
-    token* next=nullptr;
+    bool stop,next_off_range_found,need_further_inspection;
+    token* best;
     while (n--)
     {
         cin>>t>>k>>start;
@@ -39,12 +49,89 @@ int main()
     }
     for (start=1;start!=end;)
     {
-        bool stop=false;
+        stop=false;
+        next_off_range_found=false;
+        need_further_inspection=false;
         for (it=m.begin();it!=m.end();)
         {
-
+            if (it->second.tokens.rbegin()->first == start)
+            {
+                s+=it->second.tokens.rbegin()->second.text;
+                start=it->second.tokens.rbegin()->second.end;
+                if (it->second.tokens.size()==1) it=m.erase(it);
+                else it->second.tokens.erase(--(it->second.tokens.end()));
+                stop=true;break;
+            }
+            if (it->second.tokens.begin()->first == start)
+            {
+                s+=it->second.tokens.begin()->second.text;
+                start=it->second.tokens.begin()->second.end;
+                it=m.erase(it);
+                stop=true;break;
+            }
+            if (it->second.tokens.rbegin()->first > start)
+            {
+                if (!next_off_range_found)
+                {
+                    next=--(it->second.tokens.end());
+                    next_off_range_found=true;
+                }
+                else next=min_start_off_range(next,--(it->second.tokens.end()));
+                it++;
+            }
+            else if (it->second.tokens.begin()->first < start)
+            {
+                if (it->second.tokens.begin()->second.end <= start)
+                    it=m.erase(it);
+                else
+                {
+                    need_further_inspection=true;
+                    best=!best?&(it->second.tokens.begin()->second):max_end_in_range(*best,it->second.tokens.begin()->second);
+                    it=m.erase(it);
+                    break;
+                }
+            }
+            else
+            {
+                found=it->second.tokens.lower_bound(start);
+                if (found->first == start)
+                {
+                    s+=found->second.text;
+                    start=found->second.end;
+                    it->second.tokens.erase(it->second.tokens.begin(),it->second.tokens.erase(found));
+                    stop=true;break;
+                }
+                else if (found->second.end <= start)
+                {
+                    if (!next_off_range_found)
+                    {
+                        next=it->second.tokens.erase(it->second.tokens.begin(),it->second.tokens.erase(found));
+                        next_off_range_found=true;
+                    }
+                    else next=min_start_off_range(next,it->second.tokens.erase(it->second.tokens.begin(),it->second.tokens.erase(found)));
+                    it++;
+                }
+                else
+                {
+                    need_further_inspection=true;
+                    best=!best?&(found->second):max_end_in_range(*best,found->second);
+                    it++;
+                    break;
+                }
+            }
         }
-        for (++it;it!=m.end();it++)
+        if (stop) continue;
+        if (!need_further_inspection)
+        {
+            n=next->first-start;
+            while (n--) s+='a';
+            s+=next->second.text;
+            start=next->second.end;
+            auto&tokens=next->second.owner->tokens;
+            tokens.erase(next);
+            continue;
+        }
+        for (;it!=m.end();)
         {
 
         }
