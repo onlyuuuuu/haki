ebde7f8074ac7d1f6fad4e0482b956062bd0d950
Andy <duyanhnn@outlook.com>
2025-10-21 22:54:44 +0700

    commit

--- src/main/ccpp/main/05/m1_s5_4_12th_attempt_benchmarking.cpp
+++ src/main/ccpp/main/05/m1_s5_4_12th_attempt_benchmarking.cpp
@@ -14,14 +14,14 @@ struct token_comparator
     bool operator()(const int&a, const token&b) const{return a>b.start;}
     bool operator()(const token&a, const int&b) const{return a.start>b;}
 };
-const set<token,token_comparator>* min_start_max_end
-(const set<token,token_comparator>* a,const set<token,token_comparator>* b)
+map<int,token,greater<int>>* min_start_max_end
+(map<int,token,greater<int>>* a,map<int,token,greater<int>>* b)
 {
     if (!a) return b;
     if (!b) return a;
-    if (a->rbegin()->start != b->rbegin()->start)
-        return a->rbegin()->start < b->rbegin()->start ? a : b;
-    return a->rbegin()->end > b->rbegin()->end ? a : b;
+    if (a->rbegin()->second.start != b->rbegin()->second.start)
+        return a->rbegin()->second.start < b->rbegin()->second.start ? a : b;
+    return a->rbegin()->second.end > b->rbegin()->second.end ? a : b;
 }
 const token* best_extension(const token* a,const token* b)
 {
@@ -43,29 +43,101 @@ int main()
     bool stop;
     string t;int n,k,start,end=INT_MIN,id=-1;cin>>n;
     vector<string>v;v.reserve(n);
-    map<size_t,set<token,token_comparator>,greater<size_t>>m;
-    set<token,token_comparator>* tks;
-    const set<token,token_comparator>* msme;
-    set<token,token_comparator>::iterator it;
-    queue<const set<token,token_comparator>*>q;
+    map<size_t,map<int,token,greater<int>>,greater<size_t>>m;
+    map<int,token,greater<int>>* tks;
+    map<int,token,greater<int>>::iterator mi;
+    forward_list<map<int,token,greater<int>>*>l;
+    forward_list<map<int,token,greater<int>>*>::iterator li;
     const token* best;
     const token* nearest;
     while (n--)
     {
         cin>>t>>k>>start;v.push_back(t);++id;
         tks=&m.try_emplace(t.length()).first->second;
-        it=tks->emplace(start,&v[id]).first;
+        mi=tks->emplace(piecewise_construct,forward_as_tuple(start),forward_as_tuple(start,&v[id])).first;
         while (k--)
         {
             cin>>start;
-            it=tks->emplace_hint(it,start,&v[id]);
+            mi=tks->emplace_hint(mi,piecewise_construct,forward_as_tuple(start),forward_as_tuple(start,&v[id]));
         }
-        end=max(end,it->end);
+        end=max(end,mi->second.end);
     }
-    for (const auto&[key,val]:m)
+    for (auto&e:m)
     {
-        msme=min_start_max_end(msme,&val);
-        q.emplace(&val);
+        tks=min_start_max_end(tks,&e.second);
+        l.emplace_front(&e.second);
+    }
+    t=*tks->rbegin()->second.text;
+    start=tks->rbegin()->second.end;
+    tks->erase(std::prev(tks->end()));
+    while (start!=end)
+    {
+        stop=false;
+        best=nullptr;
+        nearest=nullptr;
+        for (li=l.begin();li!=l.end();)
+        {
+            tks=*li;
+            if (tks->empty() || tks->begin()->second.end <= start)
+            {
+                li=l.erase_after(std::prev(li));
+                continue;
+            }
+            if (tks->begin()->second.start == start)
+            {
+                t+=*tks->begin()->second.text;
+                start=tks->begin()->second.end;
+                li=l.erase_after(std::prev(li));
+                stop=true;break;
+            }
+            if (tks->begin()->second.start < start)
+            {
+                best=best_extension(best,&tks->begin()->second);
+                li=l.erase_after(std::prev(li));
+                break;
+            }
+            if (tks->rbegin()->second.start == start)
+            {
+                t+=*tks->rbegin()->second.text;
+                start=tks->rbegin()->second.end;
+                tks->erase(std::prev(tks->end()));
+                li++;stop=true;break;
+            }
+            if (tks->rbegin()->second.start > start)
+            {
+                nearest=nearest_neighbor(nearest,&tks->rbegin()->second);
+                li++;continue;
+            }
+            mi=tks->lower_bound(start);
+            if (mi->second.start == start)
+            {
+                t+=*mi->second.text;
+                start=mi->second.end;
+                tks->erase(mi);
+                li++;stop=true;break;
+            }
+            if (mi->second.end <= start)
+            {
+                nearest=nearest_neighbor(nearest,&mi->second);
+                li++;continue;
+            }
+            best=best_extension(best,&tks->begin()->second);
+            tks->erase(mi);
+            li++;break;
+        }
+        if (stop) continue;
+        if (!best)
+        {
+            n=nearest->start-start;
+            while (n--) t+=char_a;
+            t+=*nearest->text;
+            start=nearest->end;
+            continue;
+        }
+        while (li!=l.end())
+        {
+            
+        }
     }
     return 0;
 }
