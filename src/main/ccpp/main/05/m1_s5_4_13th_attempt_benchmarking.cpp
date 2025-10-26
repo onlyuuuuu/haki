94f71c9ae738e2a874062fac65c1f3cb7895bce6
Andy <duyanhnn@outlook.com>
2025-10-26 23:03:10 +0700

    commit

--- src/main/ccpp/main/05/m1_s5_4_13th_attempt_benchmarking.cpp
+++ src/main/ccpp/main/05/m1_s5_4_13th_attempt_benchmarking.cpp
@@ -2,8 +2,7 @@
 using namespace std;
 struct token
 {
-    const string* t;
-    int s,e;
+    const string* t;int s,e;
     token(const string*t,const int&s):t(t),s(s),e(s+t->length()){}
     bool operator==(const token&tk)const{return s==tk.s;}
     bool operator<(const token&tk)const{return s<tk.s;}
@@ -16,6 +15,14 @@ struct token
     bool operator>(const int&i)const{return s>i;}
     bool operator>=(const int&i)const{return s>=i;}
 };
+token* min_start_max_end(token*a,token*b)
+{
+    if (!a) return b;
+    if (!b) return a;
+    if (a->s != b->s)
+        return a->s < b->s ? a : b;
+    return a->e > b->e ? a : b;
+}
 static constexpr char char_a='a';
 int main()
 {
@@ -24,8 +31,9 @@ int main()
     map<size_t,vector<token>,greater<size_t>>m;
     map<size_t,vector<token>,greater<size_t>>::iterator mit;
     queue<token>q;
-    string t;int n,k,s,i,seq=-1;cin>>n;
+    string t;int n,k,s,i,seq=-1,end=INT_MIN;cin>>n;
     vector<string>d;d.reserve(n);
+    token* msme=nullptr;
     while (n--)
     {
         cin>>t>>k>>s;
@@ -34,11 +42,11 @@ int main()
         if (v.empty())
         {
             v.reserve(k);
-            v.emplace_back(&t,s);
+            v.emplace_back(&d[seq],s);
             while (--k)
             {
                 cin>>s;
-                v.emplace_back(&t,s);
+                v.emplace_back(&d[seq],s);
             }
         }
         else
@@ -46,11 +54,11 @@ int main()
             if (v.back()<s)
             {
                 v.reserve(v.size()+k);
-                v.emplace_back(&t,s);
+                v.emplace_back(&d[seq],s);
                 while (--k)
                 {
                     cin>>s;
-                    v.emplace_back(&t,s);
+                    v.emplace_back(&d[seq],s);
                 }
             }
             else
@@ -58,17 +66,21 @@ int main()
                 vector<token>tmp;
                 tmp.reserve(k+v.size());
                 for (i=0;v[i]<s;i++) tmp.push_back(v[i]);
-                tmp.emplace_back(&t,s);
+                tmp.emplace_back(&d[seq],s);
                 while (--k)
                 {
                     cin>>s;
                     for (;v[i]<s&&i<v.size();i++) tmp.push_back(v[i]);
-                    tmp.emplace_back(&t,s);
+                    tmp.emplace_back(&d[seq],s);
                 }
                 for (;i<v.size();i++) tmp.push_back(v[i]);
-                v=tmp;
+                v.swap(tmp);
             }
         }
+        msme=min_start_max_end(msme,&v[0]);
+        end=std::max(end,v.back().e);
     }
+    t=*msme->t;
+    s=msme->e;
     return 0;
 }
