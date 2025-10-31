c0ee9ca1d6b0985c0415241e8937106be15cc27e
Andy <duyanhnn@outlook.com>
2025-10-31 10:42:11 +0700

    commit

--- src/main/ccpp/main/test/pointer_test.cpp
+++ src/main/ccpp/main/test/pointer_test.cpp
@@ -5,6 +5,18 @@ struct object
     int&i;
     object();
     object(int&i):i(i){}
+    object& operator=(const int&id){i=id;return*this;}
+    object& operator=(const object*op)
+    {
+        if (!op)
+        {
+            i=0;
+            return*this;
+        }
+        i=op->i;
+        return*this;
+    }
+    bool operator==(const int&id){return i==id;}
 };
 void func(object*op)
 {
@@ -17,7 +29,12 @@ void func2(object*&op)
 int main()
 {
     int i=1;
-    func(new object(i));
-    object* p=new object(i);
-    func2(p);
+    // func(new object(i));
+    // object* p=new object(i);
+    // func2(p);
+    object o=i;
+    if ((o=2) == 2) cout<<"it works!"<<endl;
+    if ((o=nullptr) == 0) cout<<"it works again!"<<endl;
+    func(&(o=3));
+    if (o==99) cout<<"yay"<<endl;
 }
