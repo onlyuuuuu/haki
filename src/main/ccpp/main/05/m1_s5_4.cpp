973b7a1804a87c95afbbb1670c8c1a9d9a1945ff
Andy <duyanhnn@outlook.com>
2025-11-03 20:39:36 +0700

    commit

--- src/main/ccpp/main/05/m1_s5_4.cpp
+++ src/main/ccpp/main/05/m1_s5_4.cpp
@@ -1,9 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+struct token
+{
+    std::string_view txt;
+    int str=0,end=0;
+    token()=default;
+    token(const string&t,const int&s):txt(t),str(s),end(s+static_cast<int>(t.length())){}
+};
+struct input
+{
+    vector<token>::iterator frt;
+    vector<token>v;
+    input()=default;
+};
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-
+    string t;
+    cout<<t;
     return 0;
 }
