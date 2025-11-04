549f8419ab38d2d9972c10b0340a8cacce7e394a
Andy <duyanhnn@outlook.com>
2025-11-04 23:19:43 +0700

    commit

--- src/main/ccpp/main/05/m1_s5_4.cpp
+++ src/main/ccpp/main/05/m1_s5_4.cpp
@@ -9,15 +9,44 @@ struct token
 };
 struct input
 {
-    vector<token>::iterator frt;
-    vector<token>v;
+    vector<token>::iterator front;
+    vector<token>tokens;
     input()=default;
 };
+const string solve()
+{
+    map<unsigned long long,input,greater<unsigned long long>>m;
+    map<unsigned long long,input,greater<unsigned long long>>::iterator mit;
+    string t;int i,n,k,start,end=INT_MAX;cin>>n;
+    vector<string>d;d.reserve(n);
+    while(n--)
+    {
+        cin>>t>>k>>start;
+        d.emplace_back(t);
+        vector<token>&v=m.try_emplace(t.length()).first->second.tokens;
+        if(v.empty() || start >= v.back().str)
+        {
+            v.reserve(static_cast<unsigned long long>(k)+v.size());
+            v.emplace_back(d.back(),start);
+            while(--k)
+            {
+                cin>>start;
+                v.emplace_back(d.back(),start);
+            }
+            continue;
+        }
+        vector<token>tmp;
+        tmp.reserve(static_cast<unsigned long long>(k)+v.size());
+        for(i=0;v[i].str<start;i++) tmp.push_back(std::move(v[i]));
+        tmp.emplace_back(d.back(),start);
+        for(;i<static_cast<int>(v.size())&&v[i].str<start;i++)
+    }
+    return t;
+}
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    string t;
-    cout<<t;
+    cout<<solve();
     return 0;
 }
