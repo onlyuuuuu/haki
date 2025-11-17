0eac1adbf31b79b65bf802ee0b034f8293c8c077
Andy <duyanhnn@outlook.com>
2025-11-17 15:28:20 +0700

    commit

--- src/main/ccpp/main/05/m1_s5_4.cpp
+++ src/main/ccpp/main/05/m1_s5_4.cpp
@@ -91,15 +91,103 @@ int main()
         i.front=v.begin();
         end=std::max(end,v.back().end);
     }
-    optional<token>bs;entry nr;t="";
+    optional<token>bs;entry nr;t="";bool stop=false;
     for(start=1;start!=end;)
     {
+        
         bs.reset();
         nr.reset();
         for(mit=m.begin();mit!=m.end();)
         {
-            
+            if(mit->second.tokens.back().end <= start)
+            {
+                mit=m.erase(mit);
+                continue;
+            }
+            if(mit->second.tokens.back() == start)
+            {
+                t+=mit->second.tokens.back().txt;
+                start=mit->second.tokens.back().end;
+                mit=m.erase(mit);
+                stop=true;
+                break;
+            }
+            if(mit->second.tokens.back() < start)
+            {
+                bs=mit->second.tokens.back();
+                mit=m.erase(mit);
+                break;
+            }
+            if(*mit->second.front == start)
+            {
+                t+=mit->second.front->txt;
+                start=mit->second.front++->end;
+                stop=true;
+                break;
+            }
+            if(*mit->second.front > start)
+            {
+                nr=nearest_neighbor(nr,entry(mit,mit->second.front));
+                mit++;
+                continue;
+            }
+            vit=std::lower_bound(mit->second.front,mit->second.tokens.end(),start);
+            if(*vit == start)
+            {
+                t+=vit->txt;
+                start=vit->end;
+                mit->second.shift_front(++vit);
+                stop=true;
+                break;
+            }
+            if((--vit)->end > start)
+            {
+                bs=*vit;
+                mit++->second.shift_front(++vit);
+                break;
+            }
+            nr=nearest_neighbor(nr,entry(mit++,++vit));
         }
+        if(stop) continue;
+        if(!bs)
+        {
+            n=nr.n->str-start;
+            while(n--) t+='a';
+            t+=nr.n->txt;
+            start=nr.n->end;
+            nr.shift_front();
+        }
+        for(;mit!=m.end() && start + mit->first > bs->end;)
+        {
+            if(mit->second.tokens.back().end <= bs->end)
+            {
+                mit=m.erase(mit);
+                continue;
+            }
+            if(mit->second.tokens.back() <= start)
+            {
+                bs=best_extension(bs,mit->second.tokens.back());
+                mit=m.erase(mit);
+                continue;
+            }
+            if(*mit->second.front > start)
+            {
+                mit++;
+                continue;
+            }
+            if(*mit->second.front == start)
+            {
+                bs=best_extension(bs,mit++->second.front++);
+                continue;
+            }
+            vit=std::lower_bound(mit->second.front,mit->second.tokens.end(),start);
+            bs=best_extension(bs,*vit == start ? vit : --vit);
+            mit++->second.shift_front(++vit);
+        }
+        n=start-bs->str;
+        while(n--) t.pop_back();
+        t+=bs->txt;
+        start=bs->end;
     }
     cout<<t<<'\n';
     return 0;
