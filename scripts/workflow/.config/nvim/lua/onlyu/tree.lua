0e6eda62d705dfd3d460cd9f33a852e4b8774ef9
Andy <duyanhnn@outlook.com>
2025-12-21 23:42:06 +0700

    commit

--- scripts/workflow/.config/nvim/lua/onlyu/tree.lua
+++ scripts/workflow/.config/nvim/lua/onlyu/tree.lua
@@ -5,23 +5,24 @@ vim.g.loaded_netrwPlugin = 1
 -- optionally enable 24-bit colour
 vim.opt.termguicolors = true
 
-require("neo-tree").setup({
-  window = {
-    width = 30,
-  },
-  filesystem = {
-    filtered_items = {
-      visible = true, -- when true, they will just be displayed differently instead of being hidden
-      hide_dotfiles = false,
-      hide_gitignored = false,
-      hide_by_name = {
-        --".DS_Store",
-        --"thumbs.db",
-        --"node_modules",
-      },
-    },
-    follow_current_file = {
-      enabled = true,
-    },
-  },
-})
+require("nvim-tree").setup()
+-- require("neo-tree").setup({
+--   window = {
+--     width = 30,
+--   },
+--   filesystem = {
+--     filtered_items = {
+--       visible = true, -- when true, they will just be displayed differently instead of being hidden
+--       hide_dotfiles = false,
+--       hide_gitignored = false,
+--       hide_by_name = {
+--         --".DS_Store",
+--         --"thumbs.db",
+--         --"node_modules",
+--       },
+--     },
+--     follow_current_file = {
+--       enabled = true,
+--     },
+--   },
+-- })
--- scripts/workflow/.config/nvim/lua/plugins/nvim-tree2.lua
+++ scripts/workflow/.config/nvim/lua/plugins/nvim-tree2.lua
@@ -1,27 +1,14 @@
-return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons", -- optional, but recommended
-    },
-    lazy = false, -- neo-tree will lazily load itself
-    opts = {
-      window = {
-        width = 30,
-      },
-      filesystem = {
-        filtered_items = {
-          visible = true,
-          hide_dotfiles = false,
-          hide_gitignored = false,
-        },
-        follow_current_file = {
-          enabled = true,
-        },
-      },
-    }
-  }
-}
+return {}
+-- return {
+--   {
+--     "nvim-neo-tree/neo-tree.nvim",
+--     branch = "v3.x",
+--     dependencies = {
+--       "nvim-lua/plenary.nvim",
+--       "MunifTanjim/nui.nvim",
+--       "nvim-tree/nvim-web-devicons", -- optional, but recommended
+--     },
+--     lazy = false, -- neo-tree will lazily load itself
+--     enable = false
+--   }
+-- }
