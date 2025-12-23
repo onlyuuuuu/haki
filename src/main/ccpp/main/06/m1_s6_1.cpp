7bfe305f9547d724bcd29d4cecc25ab8501badb4
Andy <duyanhnn@outlook.com>
2025-12-23 12:05:56 +0700

    commit

---
+++ src/main/ccpp/main/06/m1_s6_1.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    multimap<int,int>m;
+    int n,x,i,j;
+    cin>>n>>x;
+    for(j=1;j<=n;j++)
+    {
+        cin>>i;
+        
+    }
+    return 0;
+}
