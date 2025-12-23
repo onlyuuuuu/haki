1336cff6398cd7ccaeb8fd859ffbfae5dffef760
Andy <duyanhnn@outlook.com>
2025-12-23 12:46:22 +0700

    commit

--- src/main/ccpp/main/06/m1_s6_1.cpp
+++ src/main/ccpp/main/06/m1_s6_1.cpp
@@ -2,13 +2,25 @@
 using namespace std;
 int main()
 {
-    multimap<int,int>m;
-    int n,x,i,j;
+    multimap<int,int,greater<int>>m;
+    int n,x,i;
     cin>>n>>x;
-    for(j=1;j<=n;j++)
+    for(int j=1;j<=n;j++)
     {
         cin>>i;
-        
+        m.emplace(i,j);
     }
+    auto f=m.upper_bound(x);
+    while (m.end()!=f)
+    {
+        auto f2=m.find(x-f->first);
+        if(m.end()!=f2)
+        {
+            cout<<f->first<<" "<<f2->first;
+            return 0;
+        }
+        f=m.erase(f);
+    }
+    cout<<"IMPOSSIBLE";
     return 0;
 }
