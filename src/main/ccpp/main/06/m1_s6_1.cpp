59195444c2df287970d440c9edc8ae0ae0f3dc53
Andy <duyanhnn@outlook.com>
2026-01-15 09:42:14 +0700

    commit

--- src/main/ccpp/main/06/m1_s6_1.cpp
+++ src/main/ccpp/main/06/m1_s6_1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    multimap<int,int,greater<int>>m;
+    multimap<int,int>m;
     int n,x,i;
     cin>>n>>x;
     for(int j=1;j<=n;j++)
@@ -11,11 +11,13 @@ int main()
         if (i>=x) continue;
         m.emplace(i,j);
     }
-    for (auto it=m.begin();m.end()!=it;it=m.erase(it))
+    for (auto it=m.begin();m.end()!=it;)
     {
-        auto f=m.find(x-(it->first));
+        pair<int,int>p=std::make_pair(it->first,it->second);
+        it=m.erase(it);
+        auto f=m.find(x-(p.first));
         if(m.end()==f) continue;
-        cout<<it->second<<' '<<f->second;
+        cout<<p.second<<' '<<f->second;
         return 0;
     }
     cout<<"IMPOSSIBLE";
