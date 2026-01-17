35319b11991cc15b93413613f519697be9d844c2
Andy <duyanhnn@outlook.com>
2026-01-17 18:48:12 +0700

    commit

--- src/main/java/con/ProdCons.java
+++ src/main/java/con/ProdCons.java
@@ -1,155 +1,155 @@
-import java.util.*;
-import java.util.concurrent.Semaphore;
-
-class Producer
-{
-    private final String name;
-    private volatile LinkedList<Integer> queue;
-    private final Semaphore semaphore;
-
-    public Producer(String name, LinkedList<Integer> queue, Semaphore semaphore)
-    {
-        this.name  = name;
-        this.queue = queue;
-        this.semaphore = semaphore;
-    }
-
-    public void produce()
-    {
-        if (!queue.isEmpty()) return;
-        boolean locked = semaphore.tryAcquire();
-        if (!locked) return;
-        try
-        {
-            System.out.println(name + " | " + Thread.currentThread().getName() + ": Start producing...");
+//import java.util.*;
+//import java.util.concurrent.Semaphore;
+//
+//class Producer
+//{
+//    private final String name;
+//    private volatile LinkedList<Integer> queue;
+//    private final Semaphore semaphore;
+//
+//    public Producer(String name, LinkedList<Integer> queue, Semaphore semaphore)
+//    {
+//        this.name  = name;
+//        this.queue = queue;
+//        this.semaphore = semaphore;
+//    }
+//
+//    public void produce()
+//    {
+//        if (!queue.isEmpty()) return;
+//        boolean locked = semaphore.tryAcquire();
+//        if (!locked) return;
+//        try
+//        {
+//            System.out.println(name + " | " + Thread.currentThread().getName() + ": Start producing...");
+////            while (!queue.isEmpty())
+////            {
+////                System.out.println(name + " | " + Thread.currentThread().getName() + ": Can't produce while Q is not " +
+////                                   "empty!");
+////                wait();
+////            }
+//            int i = 1;
+//            while (queue.size() < 5)
+//            {
+//                Thread.sleep(1000);
+//                queue.add(i);
+//                System.out.println(name + " | " + Thread.currentThread().getName() + ": Added: " + i++ + ". Size: " + queue.size());
+//            }
+////            notifyAll();
+//        }
+//        catch (Exception e) {}
+//        finally
+//        {
+//            if (locked) semaphore.release();
+//            System.out.println(name + " | " + Thread.currentThread().getName() + ": Lock released!");
+//        }
+//    }
+//}
+//
+//class Consumer
+//{
+//    private final String name;
+//    private volatile LinkedList<Integer> queue;
+//    private final Semaphore semaphore;
+//
+//    public Consumer(String name, LinkedList<Integer> queue, Semaphore semaphore)
+//    {
+//        this.name  = name;
+//        this.queue = queue;
+//        this.semaphore = semaphore;
+//    }
+//
+//    public void consume()
+//    {
+//        if (queue.isEmpty()) return;
+//        //System.out.println(name + " | " + Thread.currentThread().getName() + ": Queue is not empty! Gud to go!");
+//        boolean locked = semaphore.tryAcquire();
+//        if (!locked) return;
+//        try
+//        {
+//            System.out.println(name + " | " + Thread.currentThread().getName() + ": Start comsuming...");
+////            while (queue.isEmpty())
+////            {
+////                System.out.println(name + " | " + Thread.currentThread().getName() + ": Can't consume while Q is empty!");
+////                wait();
+////            }
 //            while (!queue.isEmpty())
 //            {
-//                System.out.println(name + " | " + Thread.currentThread().getName() + ": Can't produce while Q is not " +
-//                                   "empty!");
-//                wait();
+//                Thread.sleep(1000);
+//                System.out.println(name + " | " + Thread.currentThread().getName() + ": Delet: " + queue.removeLast() + ". Size: " + queue.size());
 //            }
-            int i = 1;
-            while (queue.size() < 5)
-            {
-                Thread.sleep(1000);
-                queue.add(i);
-                System.out.println(name + " | " + Thread.currentThread().getName() + ": Added: " + i++ + ". Size: " + queue.size());
-            }
-//            notifyAll();
-        }
-        catch (Exception e) {}
-        finally
-        {
-            if (locked) semaphore.release();
-            System.out.println(name + " | " + Thread.currentThread().getName() + ": Lock released!");
-        }
-    }
-}
-
-class Consumer
-{
-    private final String name;
-    private volatile LinkedList<Integer> queue;
-    private final Semaphore semaphore;
-
-    public Consumer(String name, LinkedList<Integer> queue, Semaphore semaphore)
-    {
-        this.name  = name;
-        this.queue = queue;
-        this.semaphore = semaphore;
-    }
-
-    public void consume()
-    {
-        if (queue.isEmpty()) return;
-        //System.out.println(name + " | " + Thread.currentThread().getName() + ": Queue is not empty! Gud to go!");
-        boolean locked = semaphore.tryAcquire();
-        if (!locked) return;
-        try
-        {
-            System.out.println(name + " | " + Thread.currentThread().getName() + ": Start comsuming...");
-//            while (queue.isEmpty())
+////            notifyAll();
+//        }
+//        catch (Exception e) {}
+//        finally
+//        {
+//            if (locked)
 //            {
-//                System.out.println(name + " | " + Thread.currentThread().getName() + ": Can't consume while Q is empty!");
-//                wait();
+//                semaphore.release();
+//                System.out.println(name + " | " + Thread.currentThread().getName() + ": Lock released!");
 //            }
-            while (!queue.isEmpty())
-            {
-                Thread.sleep(1000);
-                System.out.println(name + " | " + Thread.currentThread().getName() + ": Delet: " + queue.removeLast() + ". Size: " + queue.size());
-            }
-//            notifyAll();
-        }
-        catch (Exception e) {}
-        finally
-        {
-            if (locked)
-            {
-                semaphore.release();
-                System.out.println(name + " | " + Thread.currentThread().getName() + ": Lock released!");
-            }
-        }
-    }
-}
-
-// rm -rf *.class > /dev/null 2>&1; javac ProdCons.java; java ProdCons
-public class ProdCons
-{
-    private static volatile LinkedList<Integer> queue = new LinkedList<>();
-    private static Semaphore semaphore = new Semaphore(1);
-    public static void main(String[] args) throws InterruptedException
-    {
-        Producer p1 = new Producer("Producer1", queue, semaphore);
-        Producer p2 = new Producer("Producer2", queue, semaphore);
-        Consumer c1 = new Consumer("Consumer1", queue, semaphore);
-        Consumer c2 = new Consumer("Consumer2", queue, semaphore);
-        Thread t1 = new Thread(() -> {
-            while (true)
-            {
-//                try
-//                {
-//                    Thread.sleep(200);
-//                }
-//                catch (Exception e) {}
-                p1.produce();
-            }
-        }, "Thread1");
-        Thread t2 = new Thread(() -> {
-            while (true)
-            {
-//                try
-//                {
-//                    Thread.sleep(200);
-//                }
-//                catch (Exception e) {}
-                p2.produce();
-            }
-        }, "Thread2");
-        Thread t3 = new Thread(() -> {
-            while (true)
-            {
-//                try
-//                {
-//                    Thread.sleep(200);
-//                }
-//                catch (Exception e) {}
-                c1.consume();
-            }
-        }, "Thread3");
-        Thread t4 = new Thread(() -> {
-            while (true)
-            {
-//                try
-//                {
-//                    Thread.sleep(200);
-//                }
-//                catch (Exception e) {}
-                c2.consume();
-            }
-        }, "Thread4");
-        t1.start();
-        t3.start();
-        t2.start();
-        t4.start();
-    }
-}
+//        }
+//    }
+//}
+//
+//// rm -rf *.class > /dev/null 2>&1; javac ProdCons.java; java ProdCons
+//public class ProdCons
+//{
+//    private static volatile LinkedList<Integer> queue = new LinkedList<>();
+//    private static Semaphore semaphore = new Semaphore(1);
+//    public static void main(String[] args) throws InterruptedException
+//    {
+//        Producer p1 = new Producer("Producer1", queue, semaphore);
+//        Producer p2 = new Producer("Producer2", queue, semaphore);
+//        Consumer c1 = new Consumer("Consumer1", queue, semaphore);
+//        Consumer c2 = new Consumer("Consumer2", queue, semaphore);
+//        Thread t1 = new Thread(() -> {
+//            while (true)
+//            {
+////                try
+////                {
+////                    Thread.sleep(200);
+////                }
+////                catch (Exception e) {}
+//                p1.produce();
+//            }
+//        }, "Thread1");
+//        Thread t2 = new Thread(() -> {
+//            while (true)
+//            {
+////                try
+////                {
+////                    Thread.sleep(200);
+////                }
+////                catch (Exception e) {}
+//                p2.produce();
+//            }
+//        }, "Thread2");
+//        Thread t3 = new Thread(() -> {
+//            while (true)
+//            {
+////                try
+////                {
+////                    Thread.sleep(200);
+////                }
+////                catch (Exception e) {}
+//                c1.consume();
+//            }
+//        }, "Thread3");
+//        Thread t4 = new Thread(() -> {
+//            while (true)
+//            {
+////                try
+////                {
+////                    Thread.sleep(200);
+////                }
+////                catch (Exception e) {}
+//                c2.consume();
+//            }
+//        }, "Thread4");
+//        t1.start();
+//        t3.start();
+//        t2.start();
+//        t4.start();
+//    }
+//}
--- src/main/java/con/ProduceConsume.java
+++ src/main/java/con/ProduceConsume.java
@@ -1,13 +1,112 @@
-import java.util.ArrayList;
+import java.util.*;
 import java.util.concurrent.locks.*;
 
+class Broker
+{
+    public final Lock      producerLock      = new ReentrantLock();
+    public final Lock      consumerLock      = new ReentrantLock();
+    public final Condition producerCondition = producerLock.newCondition();
+    public final Condition consumerCondition = consumerLock.newCondition();
+}
 
+class Producer implements Runnable
+{
+    private final    Broker              broker;
+    private volatile LinkedList<Integer> queue;
+
+    public Producer(Broker broker, LinkedList<Integer> queue)
+    {
+        this.broker = broker;
+        this.queue  = queue;
+    }
+
+    @Override
+    public void run()
+    {
+        while (true)
+        {
+            boolean locked = false;
+            try
+            {
+                while (queue.size() > 0)
+                    broker.producerCondition.await();
+                locked = broker.producerLock.tryLock();
+                if (!locked) return;
+                int i = 1;
+                while (queue.size() < 5)
+                {
+                    Thread.sleep(1000);
+                    queue.push(i);
+                    System.out.println(Thread.currentThread().getName() + ": Added: " + i++ + ". Size: " + queue.size());
+                }
+                broker.consumerCondition.signalAll();
+            }
+            catch (Exception e) {}
+            finally
+            {
+                if (locked)
+                    broker.producerLock.unlock();
+            }
+        }
+    }
+}
+
+class Consumer implements Runnable
+{
+    private final    Broker              broker;
+    private volatile LinkedList<Integer> queue;
+
+    public Consumer(Broker broker, LinkedList<Integer> queue)
+    {
+        this.broker = broker;
+        this.queue  = queue;
+    }
+
+    @Override
+    public void run()
+    {
+        while (true)
+        {
+            boolean locked = false;
+            try
+            {
+                while (queue.isEmpty() || queue.size() != 5)
+                    broker.consumerCondition.await();
+                locked = broker.consumerLock.tryLock();
+                if (!locked) return;
+                while (queue.size() > 0)
+                {
+                    Thread.sleep(1000);
+                    System.out.println(Thread.currentThread().getName() + ": Deled: " + queue.pop() + ". Size: " + queue.size());
+                }
+                broker.producerCondition.signalAll();
+            }
+            catch (Exception e) {}
+            finally
+            {
+                if (locked)
+                    broker.consumerLock.unlock();
+            }
+        }
+    }
+}
 
 // rm -rf *.class > /dev/null 2>&1; javac ProduceConsume.java; java ProduceConsume
 public class ProduceConsume
 {
+    private static volatile LinkedList<Integer> queue = new LinkedList<>();
     public static void main(String[] args)
     {
+        Broker broker = new Broker();
+        Thread p1 = new Thread(new Producer(broker, queue), "Thead-Producer1");
+        Thread p2 = new Thread(new Producer(broker, queue), "Thead-Producer2");
+        Thread c1 = new Thread(new Consumer(broker, queue), "Thead-Consumer1");
+        Thread c2 = new Thread(new Consumer(broker, queue), "Thead-Consumer2");
 
+//        p1.start(); c1.start();
+//        p2.start(); c2.start();
+
+        p2.start(); c2.start();
+        p1.start(); c1.start();
     }
 }
