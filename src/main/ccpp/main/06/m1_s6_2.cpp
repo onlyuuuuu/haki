a67376a18a7807e4e56a99dca697eeb2248786fd
Andy <duyanhnn@outlook.com>
2026-01-18 22:05:42 +0700

    commit

--- src/main/ccpp/main/06/m1_s6_2.cpp
+++ src/main/ccpp/main/06/m1_s6_2.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 int main()
 {
-    
+    multimap<int,int>m;
+    int n,x,i,j;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        cin>>j;
+        if(j>=x-1)continue;
+        m.emplace(j,i);
+    }
+    while(m.size()>2)
+    {
+        pair<int,int>a={m.begin()->first,m.begin()->second};
+        auto bit=m.erase(m.begin());
+        pair<int,int>b={bit->first,bit->second};
+        
+    }
+    cout<<"IMPOSSIBLE"<<'\n';
     return 0;
 }
