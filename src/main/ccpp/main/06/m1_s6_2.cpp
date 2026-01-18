cc3c4fb305fcdcc2acc8d4b975347eb720f254fd
Andy <duyanhnn@outlook.com>
2026-01-18 20:39:25 +0700

    commit

--- src/main/ccpp/main/06/m1_s6_2.cpp
+++ src/main/ccpp/main/06/m1_s6_2.cpp
@@ -1,16 +1,7 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    multiset<int>s;
-    int n,x,i;
-    cin>>n>>x;
-    while(n--)
-    {
-        cin>>i;
-        if(i>=x-1)continue;
-        s.emplace(i);
-    }
     
     return 0;
 }
