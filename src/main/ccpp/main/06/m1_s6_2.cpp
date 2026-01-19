8284d224b96787cfb3f1ed5e62a7efff620aeffb
Andy <duyanhnn@outlook.com>
2026-01-19 21:47:23 +0700

    commit

--- src/main/ccpp/main/06/m1_s6_2.cpp
+++ src/main/ccpp/main/06/m1_s6_2.cpp
@@ -1,22 +1,76 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-    multimap<int,int>m;
-    int n,x,i,j;
-    cin>>n;
-    for(i=1;i<=n;i++)
+    map<int,queue<int>>m;
+    vector<pair<int,int>>v;
+    int n,x,i,ms=0;
+    cin>>n>>x;
+    if(n<3||x<3)
     {
-        cin>>j;
-        if(j>=x-1)continue;
-        m.emplace(j,i);
+        cout<<"IMPOSSIBLE"<<'\n';
+        return 0;
     }
-    while(m.size()>2)
+    for(int j=1;j<=n;j++)
     {
-        pair<int,int>a={m.begin()->first,m.begin()->second};
-        auto bit=m.erase(m.begin());
-        pair<int,int>b={bit->first,bit->second};
-        
+        cin>>i;
+        if(i>x-2)continue;
+        auto p = m.try_emplace(i,NULL);
+        if(!p.second)
+        {
+            if(p.first->second.size()==3)continue;
+            p.first->second.push(j);
+            continue;
+        }
+        queue<int>q;
+        q.push(i);
+        p.first->second=q;
+    }
+    v.emplace_back(m.begin()->first,m.begin()->second.front());
+    m.begin()->second.pop();
+    if(m.begin()->second.empty())
+        m.erase(m.begin());
+    while(!m.empty())
+    {
+        pair<int,int>p={m.begin()->first,m.begin()->second.front()};
+        m.begin()->second.pop();
+        if(m.begin()->second.empty())
+            m.erase(m.begin());
+        if(m.empty())continue;
+        int diff=ms-p.first;
+        auto vit=v.begin();
+        if(diff < v.front().first)
+            vit=v.begin();
+        else if(diff == v.front().first)
+            vit=v.begin()+1;
+        else
+            vit=std::upper_bound(v.begin(),v.end(),diff);
+        for(;vit!=v.end();vit++)
+        {
+            ms=vit->first+p.first;
+            int lookup=x-ms;
+            if(lookup == m.begin()->first)
+            {
+                cout<<vit->second<<' '<<p.second<<' '<<m.begin()->second.front()<<'\n';
+                return 0;
+            }
+            if(lookup == std::prev(m.end())->first)
+            {
+                cout<<vit->second<<' '<<p.second<<' '<<std::prev(m.end())->second.front()<<'\n';
+                return 0;
+            }
+            if(lookup > m.begin()->first && lookup < std::prev(m.end())->first)
+            {
+                auto mit=m.find(lookup);
+                if(mit!=m.end())
+                {
+                    cout<<vit->second<<' '<<p.second<<' '<<mit->second.front()<<'\n';
+                    return 0;
+                }
+            }
+        }
+        if(p.first == v.back().first)continue;
+        v.push_back(p);
     }
     cout<<"IMPOSSIBLE"<<'\n';
     return 0;
