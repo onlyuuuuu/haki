783a6e0f1cf1af029a4c5230f02e7d99e88e140b
Andy <duyanhnn@outlook.com>
2026-01-22 09:28:36 +0700

    commit

--- src/main/ccpp/main/06/m1_s6_1.cpp
+++ src/main/ccpp/main/06/m1_s6_1.cpp
@@ -1,25 +1,12 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     multimap<int,int>m;
-    int n,x,i;
+    int n,x;
     cin>>n>>x;
-    for(int j=1;j<=n;j++)
-    {
-        cin>>i;
-        if (i>=x) continue;
-        m.emplace(i,j);
-    }
-    for (auto it=m.begin();m.end()!=it;)
-    {
-        pair<int,int>p=std::make_pair(it->first,it->second);
-        it=m.erase(it);
-        auto f=m.find(x-(p.first));
-        if(m.end()==f) continue;
-        cout<<p.second<<' '<<f->second;
-        return 0;
-    }
-    cout<<"IMPOSSIBLE";
+    if(n<)
     return 0;
 }
