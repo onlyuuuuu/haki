433c8678c74bd3a6b3b404c3062815c3953499e8
Andy <duyanhnn@outlook.com>
2026-01-24 12:17:23 +0700

    commit

--- src/main/ccpp/main/06/e1.cpp
+++ src/main/ccpp/main/06/e1.cpp
@@ -4,7 +4,7 @@ struct cst_map
 {
     int s,n,p=0;
     map<int,queue<int>>m;
-    cst_map(const int&s,const int&n):s(s),n(n){}
+    cst_map(const int&s,const int&n):s(s),n(n-1){}
     auto poll()
     {
         --s;
@@ -13,32 +13,43 @@ struct cst_map
         if(m.begin()->second.empty()) m.erase(m.begin());
         return p;
     }
+    auto poph()
+    {
+        s-=m.begin()->second.size();
+        return m.erase(m.begin());
+    }
     bool push(const int&i,const int&x)
     {
-        if(i>x-n+1) return false;
+        if(i>x-n) return false;
         auto e=m.try_emplace(i);
         if(e.second)
         {
             queue<int>q;
             q.push(++p);
             e.first->second=q;
+            ++s;
             return false;
         }
-        int sum=e.first->second.size() * e.first->first + i;
+        long sum = e.first->second.size() * e.first->first + i;
         if(sum == x) return true;
         if(sum > x)  return false;
+        if(e.first->second.size() == n) return false;
         e.first->second.push(++p);
+        ++s;
         return false;
     }
 }
-queue<int> solve()
+vector<int> solve(const int&n,const int&s,cst_map&cm)
 {
-    
+    vector<int>v;
+    return v;
 }
 int main()
 {
     
     int n,x;
-    
-    queue<int>q=solve()
+    cin>>n>>x;
+    cst_map cm();
+    vector<int>v=solve();
+    return 0;
 }
