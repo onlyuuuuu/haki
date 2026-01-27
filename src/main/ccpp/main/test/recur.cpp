afe7b5b5ef1f1eb6e19a352dc76b51e8a27b5222
Andy <duyanhnn@outlook.com>
2026-01-27 19:44:28 +0700

    commit

--- src/main/ccpp/main/test/recur.cpp
+++ src/main/ccpp/main/test/recur.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 void solve(const int&n)
 {
+    cout<<n<<'\n';
     if(n==1)return;
     solve(n-1);
+    cout<<"-----------------"<<'\n';
     solve(n-1);
 }
 int main()
 {
-    solve(8);
+    solve(4);
     return 0;
 }
