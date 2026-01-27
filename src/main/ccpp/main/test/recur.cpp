db0321312abc1224cf6ee8f5445066feb978be87
Andy <duyanhnn@outlook.com>
2026-01-27 19:31:36 +0700

    commit

--- src/main/ccpp/main/test/recur.cpp
+++ src/main/ccpp/main/test/recur.cpp
@@ -1,7 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
+void solve(const int&n)
+{
+    if(n==1)return;
+    solve(n-1);
+    solve(n-1);
+}
 int main()
 {
-    
+    solve(8);
     return 0;
 }
