6c26d344ac0eabbf101310579b5d4f8a6cd82087
Andy <duyanhnn@outlook.com>
2026-05-31 13:05:21 +0700

    commit

--- src/main/ccpp/clean.sh
+++ src/main/ccpp/clean.sh
@@ -5,5 +5,4 @@ rm -rf CMakeCache.txt CMakeSettings.json
 rm -rf build/ target/ cmake-build-default/ cmake-build-debug/ CMakeFiles/
 rm -rf ccpp ccpp.exe
 rm -rf cmake_install.cmake Makefile
-cmake .
-cmake --build . --target clean
+#./cmakeBuild.sh
